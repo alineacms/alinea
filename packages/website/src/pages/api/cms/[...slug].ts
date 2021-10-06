ffe12a4e324cb3faf3c3cff70ddf922338439671
Ben Merckx <ben@codeurs.be>
2021-10-06 19:20:41 +0200

    Attempt 1000

--- packages/website/src/pages/api/cms/[...slug].ts
+++ packages/website/src/pages/api/cms/[...slug].ts
@@ -2,12 +2,15 @@ import {LocalHub, Server} from '@alinea/server'
 import {NextApiRequest, NextApiResponse} from 'next'
 import {pagesSchema} from '../../../schema'
 
+const cacheDir =
+  process.env.NODE_ENV === 'production' ? 'packages/website/' : ''
+
 const server = new Server({
   dashboardUrl: '/admin',
   hub: new LocalHub({
     schema: pagesSchema,
     contentPath: 'content',
-    cacheFile: '.env.cache'
+    cacheFile: `${cacheDir}.next/server/chunks/content`
   })
 })
 
