ebd5f56bcb18346fc9cd825df5d389c6c37d484c
Ben Merckx <ben@codeurs.be>
2021-10-06 11:43:22 +0200

    More next/webpack setup

--- packages/server/src/Server.ts
+++ packages/server/src/Server.ts
@@ -3,10 +3,11 @@ import bodyParser from 'body-parser'
 import compression from 'compression'
 import cors from 'cors'
 import express, {Router} from 'express'
-import {createServer, IncomingMessage, ServerResponse} from 'http'
+import {IncomingMessage, ServerResponse} from 'http'
 import {Socket} from 'net'
+import {nextTick} from 'process'
 import Websocket, {WebSocketServer} from 'ws'
-import {DocServer} from './ws/DocServer'
+//import {DocServer} from './ws/DocServer'
 
 export type ServerOptions = {
   dashboardUrl: string
@@ -20,25 +21,35 @@ export class Server {
 
   constructor(protected options: ServerOptions) {
     const {hub, dashboardUrl, auth} = options
+    const prefix = '(/*)?'
     const router = Router()
     router.use(compression())
     router.use(cors({origin: dashboardUrl}))
+    router.use((req, res, next) => {
+      next()
+    })
     if (auth) router.use(auth.router())
-    router.get(Api.nav.content.get(':id'), async (req, res) => {
+    router.get(prefix + Api.nav.content.get(':id'), async (req, res) => {
       const id = req.params.id
       res.json(await hub.content.get(id))
     })
-    router.get(Api.nav.content.entryWithDraft(':id'), async (req, res) => {
-      const id = req.params.id
-      const result = await hub.content.entryWithDraft(id)
-      res.json(result || null)
-    })
-    router.get(Api.nav.content.list('*'), async (req, res) => {
-      const parent = req.params['0'] ? req.params['0'] : undefined
-      res.json(await hub.content.list(parent))
-    })
+    router.get(
+      prefix + Api.nav.content.entryWithDraft(':id'),
+      async (req, res) => {
+        const id = req.params.id
+        const result = await hub.content.entryWithDraft(id)
+        res.json(result || null)
+      }
+    )
+    router.get(
+      [prefix + Api.nav.content.list('*'), prefix + Api.nav.content.list()],
+      async (req, res) => {
+        const parent = req.params['0'] ? req.params['0'] : undefined
+        res.json(await hub.content.list(parent))
+      }
+    )
     router.put(
-      Api.nav.content.entryWithDraft(':id'),
+      prefix + Api.nav.content.entryWithDraft(':id'),
       bodyParser.json(),
       async (req, res) => {
         const id = req.params.id
@@ -46,15 +57,21 @@ export class Server {
       }
     )
     router.get('*', async (req, res) => {
-      res.sendStatus(404)
+      res.status(404).json({error: 'Not found'})
     })
     this.app.use(router)
-    const docServer = new DocServer(this.options.hub)
-    this.wss.on('connection', docServer.connect)
+    //const docServer = new DocServer(this.options.hub)
+    //this.wss.on('connection', docServer.connect)
   }
 
-  respond = (req: IncomingMessage, res: ServerResponse): void =>
-    this.app(req, res)
+  respond = (req: IncomingMessage, res: ServerResponse): Promise<void> => {
+    return new Promise(resolve => {
+      res.on('finish', () => {
+        nextTick(resolve)
+      })
+      this.app(req, res)
+    })
+  }
 
   upgrade = (req: IncomingMessage, socket: Socket, head: Buffer): void => {
     const handleAuth = (ws: Websocket) => {
@@ -63,7 +80,7 @@ export class Server {
     this.wss.handleUpgrade(req, socket, head, handleAuth)
   }
 
-  listen(port: number) {
+  /*listen(port: number) {
     return createServer(this.respond).on('upgrade', this.upgrade).listen(port)
-  }
+  }*/
 }
