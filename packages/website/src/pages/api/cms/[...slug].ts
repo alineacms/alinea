4b3e56691769ae4a7f231fdd3e516e0bdd131334
Ben Merckx <ben@codeurs.be>
2022-01-28 13:21:45 +0100

    Disable next body parser

--- packages/website/src/pages/api/cms/[...slug].ts
+++ packages/website/src/pages/api/cms/[...slug].ts
@@ -58,3 +58,7 @@ const server = new Server({
 })
 
 export default server.respond
+
+// We disable the body parse that is added by Next.js as it incorrectly parses
+// application/octet-stream as string.
+export const config = {api: {bodyParser: false}}
