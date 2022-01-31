7aa8f72a80d95892ec8774c2983dcaf9cabc67f8
Ben Merckx <ben@codeurs.be>
2022-01-31 10:53:37 +0100

    Hosted Next.js provides query string encoded

--- packages/website/src/pages/api/preview.ts
+++ packages/website/src/pages/api/preview.ts
@@ -2,7 +2,7 @@ import type {NextApiRequest, NextApiResponse} from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const url = new URL(req.url!, 'http://localhost')
-  const slug = url.search.substring(1)
+  const slug = decodeURIComponent(url.search).substring(1)
   res.setPreviewData({})
   // Todo: let's sign the slug with our secret so we don't open redirects
   res.redirect(slug)
