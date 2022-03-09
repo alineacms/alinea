6d9320aadd1229dc34007eae701dc8975e7af61c
Ben Merckx <ben@codeurs.be>
2022-03-09 08:53:34 +0100

    Content

--- packages/website/src/view/blocks/TextBlock.schema.ts
+++ packages/website/src/view/blocks/TextBlock.schema.ts
@@ -3,17 +3,13 @@ import {richText} from '@alinea/input.richtext'
 import {CodeBlockSchema} from './CodeBlock.schema'
 import {ImageBlockSchema} from './ImageBlock.schema'
 
-const blocks = schema({
-  CodeBlock: CodeBlockSchema,
-  ImageBlock: ImageBlockSchema
-})
-
-const text = richText('Text', {
-  blocks
-})
-
 export const TextBlockSchema = type('Text', {
-  text
+  text: richText('Text', {
+    blocks: schema({
+      CodeBlock: CodeBlockSchema,
+      ImageBlock: ImageBlockSchema
+    })
+  })
 })
 
 export type TextBlockSchema = Schema.TypeOf<typeof TextBlockSchema>
