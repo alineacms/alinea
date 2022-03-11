be05df6e992a74d9a9c2ced0f4d585c696f5fabb
Ben Merckx <ben@codeurs.be>
2022-03-11 13:26:22 +0100

    Fix btoa impl

--- packages/ui/src/util/Base64.ts
+++ packages/ui/src/util/Base64.ts
@@ -1,10 +1,35 @@
-import {encode} from 'base64-arraybuffer'
+// Source: https://github.com/MaxArt2501/base64-js/blob/39729b0e836f86398d6ebf1fb6d70c9f307bec0b/base64.js
+
+// base64 character set, plus padding character (=)
+const b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
 
 export function btoa(input: string) {
-  var buf = new ArrayBuffer(input.length * 2)
-  var bufView = new Uint16Array(buf)
-  for (var i = 0, strLen = input.length; i < strLen; i++) {
-    bufView[i] = input.charCodeAt(i)
+  var bitmap,
+    a,
+    b,
+    c,
+    result = '',
+    i = 0,
+    rest = input.length % 3 // To determine the final padding
+
+  for (; i < input.length; ) {
+    if (
+      (a = input.charCodeAt(i++)) > 255 ||
+      (b = input.charCodeAt(i++)) > 255 ||
+      (c = input.charCodeAt(i++)) > 255
+    )
+      throw new TypeError(
+        "Failed to execute 'btoa' on 'Window': The string to be encoded contains characters outside of the Latin1 range."
+      )
+
+    bitmap = (a << 16) | (b << 8) | c
+    result +=
+      b64.charAt((bitmap >> 18) & 63) +
+      b64.charAt((bitmap >> 12) & 63) +
+      b64.charAt((bitmap >> 6) & 63) +
+      b64.charAt(bitmap & 63)
   }
-  return encode(buf)
+
+  // If there's need of padding, replace the last 'A's with equal signs
+  return rest ? result.slice(0, rest - 3) + '==='.substring(rest) : result
 }
