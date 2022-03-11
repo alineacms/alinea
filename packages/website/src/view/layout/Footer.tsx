caee7eb03310324da54b215a9e6de31c39dbf192
Ben Merckx <ben@codeurs.be>
2022-03-11 13:10:15 +0100

    Footer link size

--- packages/website/src/view/layout/Footer.tsx
+++ packages/website/src/view/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import {fromModule, Typo} from '@alinea/ui'
+import {fromModule} from '@alinea/ui'
 import Link from 'next/link'
 import css from './Footer.module.scss'
 
@@ -7,8 +7,8 @@ const styles = fromModule(css)
 export function Footer() {
   return (
     <footer className={styles.root()}>
-      <Link href="/admin" passHref>
-        <Typo.Link>Go to admin panel</Typo.Link>
+      <Link href="/admin">
+        <a className={styles.root.link()}>Go to admin panel</a>
       </Link>
     </footer>
   )
