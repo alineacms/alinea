2df97ee592d15cf70fd65e5526204d654f28b1a8
Ben Merckx <ben@codeurs.be>
2022-03-11 13:22:03 +0100

    Move sidebar into container

--- packages/website/src/view/DocPage.tsx
+++ packages/website/src/view/DocPage.tsx
@@ -13,37 +13,39 @@ const styles = fromModule(css)
 export function DocPage({title, blocks, menu, next, prev}: DocPageProps) {
   const nav = useNavTree(menu)
   return (
-    <HStack>
-      <NavSidebar>
-        <NavTree nav={nav} />
-      </NavSidebar>
-      <Container>
-        <Blocks blocks={blocks} />
-        <HStack center gap={20} className={styles.root.nav()}>
-          {prev && (
-            <Link href={prev.url} passHref>
-              <Typo.Link>
-                <HStack gap={8}>
-                  <MdArrowBack />
-                  <span>{prev.title}</span>
-                </HStack>
-              </Typo.Link>
-            </Link>
-          )}
-          <Stack.Right>
-            {next && (
-              <Link href={next.url} passHref>
+    <Container>
+      <HStack gap={80}>
+        <NavSidebar>
+          <NavTree nav={nav} />
+        </NavSidebar>
+        <div style={{flexGrow: 1}}>
+          <Blocks blocks={blocks} />
+          <HStack center gap={20} className={styles.root.nav()}>
+            {prev && (
+              <Link href={prev.url} passHref>
                 <Typo.Link>
-                  <HStack center gap={8}>
-                    <span>{next.title}</span>
-                    <MdArrowForward />
+                  <HStack gap={8}>
+                    <MdArrowBack />
+                    <span>{prev.title}</span>
                   </HStack>
                 </Typo.Link>
               </Link>
             )}
-          </Stack.Right>
-        </HStack>
-      </Container>
-    </HStack>
+            <Stack.Right>
+              {next && (
+                <Link href={next.url} passHref>
+                  <Typo.Link>
+                    <HStack center gap={8}>
+                      <span>{next.title}</span>
+                      <MdArrowForward />
+                    </HStack>
+                  </Typo.Link>
+                </Link>
+              )}
+            </Stack.Right>
+          </HStack>
+        </div>
+      </HStack>
+    </Container>
   )
 }
