be06068dcb619bc5737f63126dd34dc06e2d8291
Ben Merckx <ben@codeurs.be>
2022-04-21 14:24:45 +0200

    Minor

--- packages/core/src/Workspace.ts
+++ packages/core/src/Workspace.ts
@@ -7,13 +7,7 @@ import {getRandomColor} from './util/GetRandomColor'
 /** A record of multiple named workspaces */
 export type Workspaces = Record<string, Workspace>
 
-/**
- * Use a workspace to divide content.
- * It is possible to create internal links between workspaces.
- **/
-export type Workspace<T = any> = {
-  /** The name of the workspace */
-  name: Label
+export type WorkspaceOptions<T = any> = {
   /** The schema of the workspace */
   schema: Schema<T>
   /**
@@ -22,20 +16,31 @@ export type Workspace<T = any> = {
    * - If the source is a file, it will be evaluated.
    */
   source: string
+  /** Todo: remove or document */
+  roots: Record<string, Root<TypesOf<T>>>
   /** The directory where media files are placed in case a file backend is used */
   mediaDir?: string
   /** The main theme color used in the dashboard */
-  color: string
-  /** Todo: remove or document */
-  roots: Record<string, Root<TypesOf<T>>>
+  color?: string
   /** A react component used to preview an entry in the dashboard */
   preview?: ComponentType<{entry: T; previewToken: string}>
+}
+
+/**
+ * Use a workspace to divide content.
+ * It is possible to create internal links between workspaces.
+ **/
+export type Workspace<T = any> = Omit<WorkspaceOptions<T>, 'color'> & {
+  /** The name of the workspace */
+  name: Label
+  /** The main theme color used in the dashboard */
+  color: string
 }
 
 /** Create a workspace */
 export function workspace<T>(
   name: Label,
-  options: Omit<Workspace<T>, 'name' | 'color'> & {color?: string}
+  options: WorkspaceOptions<T>
 ): Workspace<T> {
   return {
     ...options,
