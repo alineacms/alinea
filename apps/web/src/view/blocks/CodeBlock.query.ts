0f766eab4f348c875f8203022221c4c0ecb52905
Ben Merckx <ben@codeurs.be>
2022-05-04 17:19:13 +0200

    Docs

--- apps/web/src/view/blocks/CodeBlock.query.ts
+++ apps/web/src/view/blocks/CodeBlock.query.ts
@@ -5,22 +5,29 @@ import {Pages} from '../../../.alinea/web'
 import {theme} from '../types/ShikiTheme'
 import {CodeBlockSchema} from './CodeBlock.schema'
 
+const highlighter = getHighlighter({
+  theme: {
+    ...theme,
+    type: 'light',
+    settings: [],
+    fg: '#24292f',
+    bg: '#fbf9f9'
+  },
+  langs: [{id: 'tsx', scopeName: 'source.tsx', grammar: tsxLanguage as any}]
+})
+
 export async function codeBlockQuery(pages: Pages, block: CodeBlockSchema) {
-  const highlighter = await getHighlighter({
-    theme: {
-      ...theme,
-      type: 'light',
-      settings: [],
-      fg: '#24292f',
-      bg: '#fbf9f9'
-    },
-    langs: [{id: 'tsx', scopeName: 'source.tsx', grammar: tsxLanguage as any}]
-  })
-  return {
-    ...block,
-    code:
-      block.code &&
-      highlighter.codeToHtml(block.code, {lang: block.language || 'tsx'})
+  try {
+    return {
+      ...block,
+      code:
+        block.code &&
+        (await highlighter).codeToHtml(block.code, {
+          lang: block.language || 'tsx'
+        })
+    }
+  } catch (e) {
+    return block
   }
 }
 
