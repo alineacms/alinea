638cd9fa1393161012867a1025bf325d8c2c0455
ValerieCodeurs <valerie@codeurs.be>
2022-05-13 15:04:52 +0200

    Repared number field

--- packages/input/number/src/NumberField.tsx
+++ packages/input/number/src/NumberField.tsx
@@ -1,9 +1,20 @@
 import {Field, Label, Value} from '@alinea/core'
 
 export type NumberOptions = {
+  /** Width of the field in the dashboard UI (0-1) */
+  width?: number
+  /** Add instructional text to a field */
   help?: Label
+  /** Field is optional */
+  optional?: boolean
+  /** Display a minimal version */
   inline?: boolean
+  /** A default value */
   initialValue?: number
+  /** A minimum value */
+  minValue?: number
+  /** A maximum value */
+  maxValue?: number
 }
 
 export interface NumberField extends Field.Scalar<number> {
