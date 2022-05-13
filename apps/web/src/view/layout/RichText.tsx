d66b4f9549141ba6140e4491f1cee44830f4b681
Stijn Van Doorslaer <stijn@codeurs.be>
2022-05-13 15:23:03 +0200

    RichText added align styles

--- apps/web/src/view/layout/RichText.tsx
+++ apps/web/src/view/layout/RichText.tsx
@@ -1,6 +1,6 @@
 import {TextDoc, TextNode, TypesOf} from '@alinea/core'
 import {fromModule} from '@alinea/ui'
-import {ComponentType, Fragment, ReactElement} from 'react'
+import {ComponentType, CSSProperties, Fragment, ReactElement} from 'react'
 import reactStringReplace from 'react-string-replace'
 import css from './RichText.module.scss'
 
@@ -15,7 +15,12 @@ function getElement(
       const Tag = `h${attributes?.level || 1}` as 'h1'
       return <Tag className={styles.heading()} />
     case 'paragraph':
-      return <p className={styles.paragraph()} />
+      return (
+        <p
+          className={styles.paragraph()}
+          style={richTextStyles(attributes as RichTextAttributes | undefined)}
+        />
+      )
     case 'bold':
       return <b />
     case 'italic':
@@ -26,6 +31,23 @@ function getElement(
       return <ol className={styles.list()} />
     case 'listItem':
       return <li className={styles.listItem()} />
+  }
+}
+
+type RichTextAttributes = {
+  textAlign: 'center' | 'right' | 'justify'
+}
+
+function richTextStyles(attrs: RichTextAttributes | undefined): CSSProperties {
+  switch (true) {
+    case attrs?.textAlign === 'center':
+      return {textAlign: 'center'}
+    case attrs?.textAlign === 'right':
+      return {textAlign: 'right'}
+    case attrs?.textAlign === 'justify':
+      return {textAlign: 'justify'}
+    default:
+      return {}
   }
 }
 
