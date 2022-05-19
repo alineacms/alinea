10d2be9c92ed68736e4c813ac343e611a9f68ffa
ValerieCodeurs <valerie@codeurs.be>
2022-05-19 13:08:57 +0200

    Added workspace preference and update functions

--- packages/ui/src/hook/UsePreferences.ts
+++ packages/ui/src/hook/UsePreferences.ts
@@ -1,12 +1,23 @@
 import {createContext, useContext} from 'react'
 
+export type ColorScheme = 'light' | 'dark' | undefined
+export type FontSize = 'small' | 'medium' | 'large' | undefined
+export type Language = 'en' | undefined
+
 export type Preferences = {
-  scheme: 'light' | 'dark' | undefined
-  size: 'small' | 'medium' | 'large' | undefined
-  language: 'en' | undefined
+  scheme: ColorScheme
+  workspace: string | undefined
+  size: FontSize
+  language: Language
 }
 
-export type PreferencesContext = [Preferences, () => void]
+export type PreferencesContext = [
+  Preferences,
+  () => void,
+  (workspace: string) => void,
+  (size: FontSize) => void,
+  (lang: Language) => void
+]
 
 const context = createContext<PreferencesContext | undefined>(undefined)
 
