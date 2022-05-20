3c460f5abee849c19660ca1b065b7ee04262906e
ValerieCodeurs <valerie@codeurs.be>
2022-05-20 12:59:12 +0200

    Use a ref + effect to query only children

--- apps/web/src/view/ChangelogView.tsx
+++ apps/web/src/view/ChangelogView.tsx
@@ -1,5 +1,5 @@
 import {fromModule} from '@alinea/ui'
-import {useEffect, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import css from './ChangelogView.module.scss'
 
 const styles = fromModule(css)
@@ -10,41 +10,48 @@ export default function ChangelogView(props: ChangelogViewProps) {
   const [stickyHeading, setStickyHeading] = useState<number | undefined>(
     undefined
   )
+  const divRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const links = document.getElementsByTagName('a')
-    Array.from(links)
-      .filter(
-        el =>
-          !el.classList.value.startsWith('Header') &&
-          !el.href.startsWith(window.location.origin)
-      )
-      .map(el => el.setAttribute('target', '_blank'))
-  }, [])
-
-  useEffect(() => {
-    const headings = document.getElementsByTagName('h2')
-    const onScroll = e => {
-      const topHeading = Array.from(headings)
-        .map((el, index) => ({id: index, top: el.getBoundingClientRect().top}))
-        .filter(el => el.top >= 0 && el.top < 180)
-      if (topHeading.length === 0) return
-      setStickyHeading(topHeading[topHeading.length - 1].id)
+    if (divRef.current) {
+      const links = divRef.current.querySelectorAll('a')
+      Array.from(links)
+        .filter(el => !el.href.startsWith(window.location.origin))
+        .map(el => el.setAttribute('target', '_blank'))
     }
-    window.addEventListener('scroll', onScroll)
-
-    return () => window.removeEventListener('scroll', onScroll)
-  }, [])
+  }, [divRef])
 
   useEffect(() => {
-    const headings = document.getElementsByTagName('h2')
-    Array.from(headings).map(el => el.classList.remove('is-sticky'))
-    stickyHeading !== undefined &&
-      headings[stickyHeading].classList.add('is-sticky')
-  }, [stickyHeading])
+    if (divRef.current) {
+      const headings = divRef.current.querySelectorAll('h2')
+      const onScroll = () => {
+        const topHeading = Array.from(headings)
+          .map((el, index) => ({
+            id: index,
+            top: el.getBoundingClientRect().top
+          }))
+          .filter(el => el.top >= 0 && el.top < 180)
+        if (topHeading.length === 0) return
+        setStickyHeading(topHeading[topHeading.length - 1].id)
+      }
+      window.addEventListener('scroll', onScroll)
+
+      return () => window.removeEventListener('scroll', onScroll)
+    }
+  }, [divRef])
+
+  useEffect(() => {
+    if (divRef.current) {
+      const headings = divRef.current.querySelectorAll('h2')
+      Array.from(headings).map(el => el.classList.remove('is-sticky'))
+      stickyHeading !== undefined &&
+        headings[stickyHeading].classList.add('is-sticky')
+    }
+  }, [divRef, stickyHeading])
 
   return (
     <div
+      ref={divRef}
       className={styles.root()}
       dangerouslySetInnerHTML={{__html: props.content}}
     />
