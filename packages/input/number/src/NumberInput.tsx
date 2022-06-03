fbd3e2f03036538ee332b1dc80d66c7f576634b6
ValerieCodeurs <86597952+ValerieCodeurs@users.noreply.github.com>
2022-06-03 16:41:03 +0200

    Repared number field (#105)

--- packages/input/number/src/NumberInput.tsx
+++ packages/input/number/src/NumberInput.tsx
@@ -1,7 +1,9 @@
-import {InputLabel, InputState} from '@alinea/editor'
-import {fromModule} from '@alinea/ui'
+import {InputLabel, InputState, useInput} from '@alinea/editor'
+
+import IcRoundNumbers from '@alinea/ui/icons/IcRoundNumbers'
 import {NumberField} from './NumberField'
 import css from './NumberInput.module.scss'
+import {fromModule} from '@alinea/ui'
 
 const styles = fromModule(css)
 
@@ -10,11 +12,28 @@ export type NumberInputProps = {
   field: NumberField
 }
 
-export function NumberInput({field}: NumberInputProps) {
+export function NumberInput({state, field}: NumberInputProps) {
+  const {inline, help, optional, initialValue, width, minValue, maxValue} =
+    field.options
+  const [value = initialValue, setValue] = useInput(state)
   return (
     <div>
-      <InputLabel label={field.label}>
-        <input type="number" />
+      <InputLabel
+        asLabel
+        label={field.label}
+        inline={inline}
+        help={help}
+        optional={optional}
+        width={width}
+        icon={IcRoundNumbers}
+      >
+        <input
+          type="number"
+          className={styles.root.input()}
+          onChange={e => setValue(Number(e.currentTarget.value))}
+          min={minValue}
+          max={maxValue}
+        />
       </InputLabel>
     </div>
   )
