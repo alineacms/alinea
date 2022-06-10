fa89fde29e970c4a91cbd1bab477ffbb5381cc0e
Stijn Van Doorslaer <stijn@codeurs.be>
2022-06-10 09:59:35 +0200

    Added copyToClipboard on CodeBlock

--- apps/web/src/view/blocks/CodeBlock.tsx
+++ apps/web/src/view/blocks/CodeBlock.tsx
@@ -1,18 +1,36 @@
 import {fromModule, Typo} from '@alinea/ui'
+import {IcRoundCheck} from '@alinea/ui/icons/IcRoundCheck'
+import {useState} from 'react'
+import MdiContentCopy from '../../icons/MdiContentCopy'
 import css from './CodeBlock.module.scss'
 import {CodeBlockSchema} from './CodeBlock.schema'
 
 const styles = fromModule(css)
 
 export function CodeBlock({code, compact, fileName}: CodeBlockSchema) {
+  const [isCopied, setIsCopied] = useState(false)
+
+  function handleCopyToClipboard(content) {
+    navigator.clipboard.writeText(content)
+    setIsCopied(true)
+    setTimeout(() => setIsCopied(false), 2500)
+  }
+
   return (
-    <div className={styles.root({compact})}>
+    <div className={styles.root({compact, copied: isCopied})}>
       {fileName && <div className={styles.root.fileName()}>{fileName}</div>}
       <Typo.Monospace
         as="div"
         dangerouslySetInnerHTML={{__html: code}}
         className={styles.root.code()}
       />
+      <button
+        onClick={() => handleCopyToClipboard(code)}
+        className={styles.root.copy()}
+      >
+        {!isCopied && <MdiContentCopy style={{fontSize: '18px'}} />}
+        {isCopied && <IcRoundCheck style={{fontSize: '24px'}} />}
+      </button>
     </div>
   )
 }
