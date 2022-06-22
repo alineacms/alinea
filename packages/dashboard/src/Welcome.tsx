b08954d27383ea85bb2c83526071af1312e6853a
Ben Merckx <ben@codeurs.be>
2022-06-22 10:36:55 +0200

    Fix docs link

--- packages/dashboard/src/Welcome.tsx
+++ packages/dashboard/src/Welcome.tsx
@@ -5,7 +5,7 @@ export function Welcome() {
     <VStack gap={20} style={{marginTop: px(20), padding: px(6)}}>
       <p>Your alinea installation is now ready for configuration.</p>
       <Typo.Link
-        href="https://alinea.sh/docs/overview/configuration"
+        href="https://alinea.sh/docs/configuration/intro"
         target="_blank"
       >
         Learn how to configure
