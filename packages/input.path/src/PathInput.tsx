b5bac354f62916240fff8b641448c9b5d31b7deb
Dimitri <dimitri@codeurs.be>
2022-07-12 16:16:05 +0200

    unique paths wip

--- packages/input.path/src/PathInput.tsx
+++ packages/input.path/src/PathInput.tsx
@@ -1,8 +1,10 @@
-import {isSeparator, slugify} from '@alinea/core'
+import {Entry, isSeparator, Outcome, slugify} from '@alinea/core'
+import {useCurrentDraft, useSession} from '@alinea/dashboard'
 import {InputLabel, InputState, useInput} from '@alinea/editor'
-import {fromModule} from '@alinea/ui'
+import {fromModule, HStack} from '@alinea/ui'
 import {IcRoundLink} from '@alinea/ui/icons/IcRoundLink'
 import {useState} from 'react'
+import {useQuery} from 'react-query'
 import {PathField} from './PathField'
 import css from './PathInput.module.scss'
 
@@ -26,6 +28,53 @@ export function PathInput({state, field}: PathInputProps) {
   const [endsWithSeparator, setEndsWithSeparator] = useState(false)
   const inputValue = (value || '') + (endsWithSeparator ? '-' : '')
   const empty = value === ''
+
+  const {hub} = useSession()
+  const draft = useCurrentDraft()
+  // WIP:
+  type Paths = Array<{id: string; path: string}>
+  function getPaths() {
+    const isParent = Entry.parent.is(draft.parent)
+    const isExact = Entry.path.is(inputValue)
+    const startsWith = Entry.path.like(inputValue + '-%')
+    const isNotSelf = Entry.id.isNot(draft.id)
+    const condition = isParent.and(isNotSelf).and(isExact.or(startsWith))
+    return hub
+      .query({
+        cursor: Entry.where(condition).select({id: Entry.id, path: Entry.path})
+      })
+      .then(Outcome.unpack)
+  }
+  const {data} = useQuery(
+    ['path', draft.parent, draft.id, inputValue],
+    getPaths
+  )
+
+  function getSuffix(data: Paths) {
+    if (!data.length) return
+    const otherPaths = data.map(d => d.path)
+    if (otherPaths.includes(inputValue)) {
+      let suffix = 0
+      while (true)
+        if (!otherPaths.includes(`${inputValue}-${++suffix}`)) return suffix
+    }
+  }
+
+  async function applySuffix() {
+    const pathData = await getPaths()
+    const suffix = getSuffix(pathData)
+    if (!suffix) return
+    setValue(slugify(`${inputValue}-${suffix}`))
+  }
+
+  /* Todo:
+      - Server side
+      - Looks
+      - On create new (path field)
+  */
+
+  const currentSuffix = data && getSuffix(data)
+
   return (
     <InputLabel
       asLabel
@@ -37,19 +86,25 @@ export function PathInput({state, field}: PathInputProps) {
       icon={IcRoundLink}
       empty={empty}
     >
-      <input
-        className={styles.root.input()}
-        type="text"
-        value={inputValue}
-        onChange={e => {
-          const value = e.currentTarget.value
-          setEndsWithSeparator(isSeparator(value.charAt(value.length - 1)))
-          setValue(slugify(value))
-        }}
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
-        placeholder={' '}
-      />
+      <HStack center gap={15}>
+        <input
+          className={styles.root.input()}
+          type="text"
+          value={inputValue}
+          onChange={e => {
+            const value = e.currentTarget.value
+            setEndsWithSeparator(isSeparator(value.charAt(value.length - 1)))
+            setValue(slugify(value))
+          }}
+          onFocus={() => setFocus(true)}
+          onBlur={() => {
+            setFocus(false)
+            applySuffix()
+          }}
+          placeholder={' '}
+        />
+        <div>{currentSuffix ? `-${currentSuffix}` : null}</div>
+      </HStack>
     </InputLabel>
   )
 }
