2442e7926a9e88b3b2d87df6b618683ae0348296
Ben Merckx <ben@codeurs.be>
2022-07-19 16:32:02 +0200

    Add some extra metadata to external links

--- packages/core/src/Reference.ts
+++ packages/core/src/Reference.ts
@@ -2,5 +2,11 @@ export type Reference = Reference.Entry | Reference.Url
 
 export namespace Reference {
   export type Entry = {id: string; type: 'entry'; entry: string}
-  export type Url = {id: string; type: 'url'; url: string}
+  export type Url = {
+    id: string
+    type: 'url'
+    url: string
+    description?: string
+    target?: '_blank' | '_self'
+  }
 }
