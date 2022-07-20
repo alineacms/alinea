92fdb10fcbe26b11fede2875d5ce85ea533d6505
dmerckx <david@codeurs.be>
2022-07-20 16:12:43 +0200

    update handshake route

--- packages/cloud/src/server/CloudConfig.ts
+++ packages/cloud/src/server/CloudConfig.ts
@@ -5,7 +5,7 @@ export const cloudConfig = {
   url: baseUrl,
   jwks: `${baseUrl}/.well-known/jwks.json`,
   auth: `${baseUrl}/auth`,
-  handshake: `${baseUrl}/api/handshake`,
+  handshake: `${baseUrl}/api/v1/handshake`,
   publish: `${baseUrl}/api/v1/publish`,
   draft: `${baseUrl}/api/v1/draft`,
   media: `${baseUrl}/api/v1/media`
