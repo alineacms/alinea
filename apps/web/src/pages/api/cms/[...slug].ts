b18799c8d386c7dd0b04b138e3e433a85f875434
Ben Merckx <ben@codeurs.be>
2022-07-28 10:13:25 +0200

    Disable experimental-edge runtime for now

--- apps/web/src/pages/api/cms/[...slug].ts
+++ apps/web/src/pages/api/cms/[...slug].ts
@@ -1,15 +1,26 @@
+import {nodeHandler} from '@alinea/backend/router/NodeHandler'
 import {backend} from '@alinea/content/backend.js'
-import {Request, Response} from '@alinea/iso'
 
+export default nodeHandler(backend.handle)
+
+/*
 export default async (req: Request) => {
   const response = await backend.handle(req)
   if (response === undefined) return new Response('Not found', {status: 404})
   return response
 }
+*/
 
 export const config = {
-  runtime: 'experimental-edge',
-  // We disable the body parser that is added by Next.js as it incorrectly parses
-  // application/octet-stream as string.
+  // Unfortunately we can't enable this just yet, the edge runtime cannot be
+  // conditionally targeted (vercel/next.js#39075).
+  // We also have a few more places where client code leaks into server side
+  // (see MediaSchema view, and CloudAuthView)
+
+  // runtime: 'experimental-edge',
+
+  // We disable the body parser that is added by Next.js as it incorrectly
+  // parses application/octet-stream as string.
+
   api: {bodyParser: false}
 }
