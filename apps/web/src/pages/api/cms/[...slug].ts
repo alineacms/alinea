c113e02c4d39a6baa045e20b76ecfe299e6c0b6f
Ben Merckx <ben@codeurs.be>
2022-08-09 13:49:24 +0200

    Revert edge route

--- apps/web/src/pages/api/cms/[...slug].ts
+++ apps/web/src/pages/api/cms/[...slug].ts
@@ -1,12 +1,15 @@
+import {nodeHandler} from '@alinea/backend/router/NodeHandler'
 import {backend} from '@alinea/content/backend.js'
 
-//export default nodeHandler(backend.handle)
+export default nodeHandler(backend.handle)
 
+/*
 export default async (req: Request) => {
   const response = await backend.handle(req)
   if (response === undefined) return new Response('Not found', {status: 404})
   return response
 }
+*/
 
 export const config = {
   // Unfortunately we can't enable this just yet, the edge runtime cannot be
@@ -14,7 +17,7 @@ export const config = {
   // We also have a few more places where client code leaks into server side
   // (see MediaSchema view, and CloudAuthView)
 
-  runtime: 'experimental-edge',
+  // runtime: 'experimental-edge',
 
   // We disable the body parser that is added by Next.js as it incorrectly
   // parses application/octet-stream as string.
