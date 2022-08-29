371c4a0d5510ffc0acd4010dd7cc47386369a3cc
Ben <ben@codeurs.be>
2022-08-29 09:58:00 +0200

    Use wireit to define build command dependencies (#244)

--- .esbx/plugin/dist.ts
+++ .esbx/plugin/dist.ts
@@ -9,12 +9,15 @@ export const distPlugin: Plugin = {
     build.initialOptions.metafile = true
     build.onEnd(async res => {
       if (res.errors.length > 0) return
-      const packages = glob.sync('dist/**/*/src')
+      const packages = glob.sync('dist/out/**/*/src')
       for (const pkg of packages) {
-        const dist = pkg.replace('dist/', 'packages/').replace('/src', '/dist')
+        const dist = pkg
+          .replace('dist/out/', 'packages/')
+          .replace('/src', '/dist')
         await copyDir(pkg, dist)
+        await copyDir(pkg.replace('/out/', '/types/'), dist)
       }
-      await fs.copyFile('dist/index.css', 'packages/css/src/generated.css')
+      await fs.copyFile('dist/out/index.css', 'packages/css/src/generated.css')
     })
   }
 }
