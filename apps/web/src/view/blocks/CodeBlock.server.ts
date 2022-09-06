9d6d8c62971e7810ad4659dd81b6e5130c41381f
Ben Merckx <ben@codeurs.be>
2022-09-06 12:02:17 +0200

    Only load shiki if required

--- apps/web/src/view/blocks/CodeBlock.server.ts
+++ apps/web/src/view/blocks/CodeBlock.server.ts
@@ -1,29 +1,42 @@
 import type {Pages} from '@alinea/backend'
 import {Expr} from '@alinea/store'
-import {getHighlighter} from 'shiki'
+import type {Highlighter} from 'shiki'
 import languageShellScript from '../types/ShikiBashLanguage'
 import {theme} from '../types/ShikiTheme'
 import languageTsx from '../types/ShikiTsxLanguage'
 
-const highlighter = getHighlighter({
-  theme: {
-    ...theme,
-    type: 'light',
-    settings: [],
-    fg: '#1E232A',
-    bg: 'white',
-    colors: {...theme.colors, 'editor.background': 'var(--web-code-background)'}
-  },
-  langs: [
-    {id: 'tsx', scopeName: 'source.tsx', grammar: languageTsx},
-    {id: 'shellscript', scopeName: 'source.shell', grammar: languageShellScript}
-  ]
-})
+async function loadHighlighter() {
+  const {getHighlighter} = await import('shiki')
+  return getHighlighter({
+    theme: {
+      ...theme,
+      type: 'light',
+      settings: [],
+      fg: '#1E232A',
+      bg: 'white',
+      colors: {
+        ...theme.colors,
+        'editor.background': 'var(--web-code-background)'
+      }
+    },
+    langs: [
+      {id: 'tsx', scopeName: 'source.tsx', grammar: languageTsx},
+      {
+        id: 'shellscript',
+        scopeName: 'source.shell',
+        grammar: languageShellScript
+      }
+    ]
+  })
+}
+
+let highlighter: Promise<Highlighter>
 
 export function transformCode(field: Expr<string>, pages: Pages<any>) {
   return pages.process(field, async code => {
     if (!code) return ''
-    const {codeToHtml} = await highlighter
+    const {codeToHtml} = await (highlighter ||
+      (highlighter = loadHighlighter()))
     return codeToHtml(code, {
       lang: code.startsWith('#') ? 'shellscript' : 'tsx'
     })
