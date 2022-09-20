32d3847943ae1a719e770b2a94978e3e1cc86616
Ben <ben@codeurs.be>
2022-09-20 12:23:04 +0200

    Add alinea to the next.js blog starter example (#261)

---
+++ examples/framework-next/schema/author.ts
@@ -0,0 +1,9 @@
+import alinea from 'alinea'
+
+export const Author = alinea.type('Author', {
+  title: alinea.text('Name'),
+  path: alinea.path('Path', {hidden: true}),
+  picture: alinea.image('Image')
+})
+
+export type Author = alinea.infer<typeof Author>
---
+++ examples/framework-next/schema/index.ts
@@ -0,0 +1,4 @@
+export * from './author'
+export * from './blog-container'
+export * from './blog-post'
+export * from './home-page'
