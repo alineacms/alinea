c4bef224665c32bad8814307f0375ab6af533826
Ben Merckx <ben@codeurs.be>
2022-09-28 16:27:12 +0200

    Add counter

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '@alinea/css'
 import '@alinea/web/src/styles/global.scss'
 import type {AppProps} from 'next/app'
+import Script from 'next/script'
 import type {PageViewProps} from '../view/PageView.server'
 
 export function App({Component, pageProps}: AppProps<PageViewProps>) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://cdn.counter.dev/script.js"
+        data-id="add5d16a-04cf-484e-9f22-4d648ee47a95"
+        data-utcoffset="2"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
