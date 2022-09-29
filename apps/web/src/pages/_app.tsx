dc8099aa419cfad46f52e08893dc471d53ad90cd
Ben Merckx <ben@codeurs.be>
2022-09-29 13:10:06 +0200

    Pass title

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -8,7 +8,7 @@ import {SEO} from '../view/SEO'
 export function App({Component, pageProps}: AppProps<PageViewProps>) {
   return (
     <>
-      <SEO title={pageProps.title} />
+      <SEO title={pageProps.entry.title} />
       <Script
         src="https://cdn.counter.dev/script.js"
         data-id="add5d16a-04cf-484e-9f22-4d648ee47a95"
