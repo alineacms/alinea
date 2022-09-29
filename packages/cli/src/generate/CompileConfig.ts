d24ef1c400c93d57a984adafe3a5951a3b8743e9
Ben Merckx <ben@codeurs.be>
2022-09-29 16:25:00 +0200

    Don't attempt to read tsconfig files - they require a jsonc parser

--- packages/cli/src/generate/CompileConfig.ts
+++ packages/cli/src/generate/CompileConfig.ts
@@ -19,20 +19,14 @@ function overrideTsConfig(cwd: string): string | undefined {
   if (hasTsConfig) {
     // Unfortunately the only way to overwrite the jsx setting is to provide
     // esbuild with a path to another tsconfig file within the same dir
-    const source = fs.readJSONSync(tsConfig)
-    // This is not entirely correct as it could be extending another tsconfig
-    // that has this setting but we'll fix that if anyone every runs into it
-    if (source.compilerOptions?.jsx !== 'react-jsx') {
-      const extendedConfig = {
-        extends: './tsconfig.json',
-        compilerOptions: {jsx: 'react-jsx'}
-      }
-      fs.writeFileSync(
-        overrideLocation,
-        JSON.stringify(extendedConfig, null, 2)
-      )
-      return overrideLocation
+    // We used to check the jsx value here before, but it requires a bunch of
+    // dependencies to read it correctly, so always overriding is much easier
+    const extendedConfig = {
+      extends: './tsconfig.json',
+      compilerOptions: {jsx: 'react-jsx'}
     }
+    fs.writeFileSync(overrideLocation, JSON.stringify(extendedConfig, null, 2))
+    return overrideLocation
   }
 }
 
