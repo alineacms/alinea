ca2e29b5108d436d69176066994037304b75e4b6
Ben Merckx <ben@codeurs.be>
2022-10-11 16:24:56 +0200

    Export dashboard and handler for the remix example

--- examples/framework-remix/alinea.config.tsx
+++ examples/framework-remix/alinea.config.tsx
@@ -1,16 +1,25 @@
+import {createCloudBackend} from '@alinea/cloud'
 import {IcRoundInsertDriveFile} from '@alinea/ui/icons/IcRoundInsertDriveFile'
 import {IcRoundPermMedia} from '@alinea/ui/icons/IcRoundPermMedia'
 import {alinea, BrowserPreview, MediaSchema} from 'alinea'
 
+const schema = alinea.schema({
+  ...MediaSchema,
+  Welcome: alinea.type('Welcome', {
+    title: alinea.text('Title', {width: 0.5}),
+    path: alinea.path('Path', {width: 0.5}),
+    body: alinea.richText('Body text')
+  })
+})
+
 export const config = alinea.createConfig({
-  schema: alinea.schema({
-    ...MediaSchema,
-    Welcome: alinea.type('Welcome', {
-      title: alinea.text('Title', {width: 0.5}),
-      path: alinea.path('Path', {width: 0.5}),
-      body: alinea.richText('Body text')
-    })
-  }),
+  schema,
+  dashboard: {
+    staticFile: './public/admin.html',
+    dashboardUrl: '/admin.html',
+    handlerUrl: '/cms'
+  },
+  backend: createCloudBackend(),
   workspaces: {
     main: alinea.workspace('Remix example', {
       source: './content',
