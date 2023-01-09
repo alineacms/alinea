f6b451cb4866295c82714ae6fde110fd05017c07
Ben Merckx <ben@codeurs.be>
2023-01-09 17:15:43 +0100

    Move Preview exports to the preview package

--- packages/alinea/src/index.ts
+++ packages/alinea/src/index.ts
@@ -3,8 +3,6 @@ export {Root, Tree, Workspace} from '@alinea/core'
 export type {Config, Schema, TextDoc, Type} from '@alinea/core'
 // Dashboard
 export {MediaSchema} from '@alinea/dashboard/schema/MediaSchema'
-export {Preview} from '@alinea/dashboard/view/Preview'
-export {BrowserPreview} from '@alinea/dashboard/view/preview/BrowserPreview'
 export {alinea}
 import * as alinea from './alinea'
 export default alinea
