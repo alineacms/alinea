a1f938e2cee0b01805445a0609a3a50109f56942
Ben Merckx <ben@codeurs.be>
2023-02-28 11:46:52 +0100

    Concat css

--- src/dev/css.ts
+++ src/dev/css.ts
@@ -1,57 +1,24 @@
 import type {Plugin} from 'esbuild'
 import fs from 'node:fs'
-import path from 'node:path'
-import {sassPlugin} from './sass.js'
 
 export const cssPlugin: Plugin = {
   name: 'css',
   setup(build) {
-    const {
-      outfile,
-      outdir = path.dirname(outfile!),
-      absWorkingDir = process.cwd()
-    } = build.initialOptions
-    const outputDir = path.isAbsolute(outdir)
-      ? outdir
-      : path.join(absWorkingDir, outdir)
     build.initialOptions.metafile = true
     build.onEnd(async res => {
       if (res.errors.length > 0) return
       const meta = res.metafile!
-      const files = Object.keys(meta.inputs).filter(file => {
-        return file.endsWith('.scss')
+      const files = Object.entries(meta.outputs).filter(([file]) => {
+        return file.endsWith('.css')
       })
-      const input =
-        `import './src/global.scss'\n` +
+      const contents =
+        fs.readFileSync('src/global.css', 'utf-8') +
         files
-          .filter(file => !file.startsWith('@esbx'))
-          .map(file => {
-            const loc = path
-              .relative(absWorkingDir, file)
-              .split(path.sep)
-              .join('/')
-            return `import './${loc}'`
+          .map(([file]) => {
+            return fs.readFileSync(file, 'utf-8')
           })
           .join('\n')
-      return build.esbuild
-        .build({
-          ignoreAnnotations: true,
-          outfile: 'dist/index.js',
-          stdin: {contents: input, resolveDir: absWorkingDir},
-          plugins: [sassPlugin],
-          write: false,
-          bundle: true,
-          absWorkingDir,
-          loader: {
-            '.woff2': 'file'
-          }
-        })
-        .then(res => {
-          for (const output of res.outputFiles) {
-            if (output.path.endsWith('.js')) continue
-            fs.writeFileSync(output.path, output.contents)
-          }
-        })
+      fs.writeFileSync('dist/index.css', contents)
     })
   }
 }
