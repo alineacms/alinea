40c3424c629dac7517bb9c3cbb6c00d06e63b9a6
Ben Merckx <ben@codeurs.be>
2023-03-08 08:39:34 +0100

    Interim

--- src/backend/db/Database.test.ts
+++ src/backend/db/Database.test.ts
@@ -1,66 +1,107 @@
 import sqlite from '@alinea/sqlite-wasm'
+import {createConfig, root, schema, type, workspace} from 'alinea/core'
+import {path, text} from 'alinea/input'
 import {connect} from 'rado/driver/sql.js'
 import {test} from 'uvu'
-import {Database, EntryRaw} from './Database.js'
+import {Database, FileInfo} from './Database.js'
 
-const entry1: EntryRaw = {
-  lastModified: Date.now(),
-  id: 'root',
-  type: 'Type',
-  title: 'Test title',
-  path: '/',
-  url: '/',
-  alinea: {
-    index: 'a0',
-    parent: undefined,
-    parents: [],
-    workspace: 'main',
-    root: 'data'
-  }
+const encode = (data: any) => new TextEncoder().encode(JSON.stringify(data))
+
+const entry1: FileInfo = {
+  modifiedAt: Date.now(),
+  workspace: 'main',
+  root: 'data',
+  filePath: 'index.json',
+  contents: encode({
+    id: 'root',
+    type: 'Type',
+    title: 'Test title',
+    path: '/',
+    url: '/',
+    alinea: {
+      index: 'a0',
+      parent: undefined,
+      parents: []
+    }
+  })
 }
 
-const entry2: EntryRaw = {
-  lastModified: Date.now(),
-  id: 'entry2',
-  type: 'Type',
-  title: 'Test title',
-  path: '/entry2',
-  url: '/entry2',
-  alinea: {
-    index: 'a0',
-    parent: entry1.id,
-    parents: [entry1.id],
-    workspace: 'main',
-    root: 'data'
-  }
+const entry2: FileInfo = {
+  modifiedAt: Date.now(),
+  workspace: 'main',
+  root: 'data',
+  filePath: 'index/entry2.json',
+  contents: encode({
+    id: 'entry2',
+    type: 'Type',
+    title: 'Entry 2',
+    path: '/entry2',
+    url: '/entry2',
+    alinea: {
+      index: 'a0',
+      parent: undefined,
+      parents: []
+    }
+  })
 }
 
-const entry3: EntryRaw = {
-  lastModified: Date.now(),
-  id: 'entry3',
-  type: 'Type',
-  title: 'Test title',
-  path: '/entry3',
-  url: '/entry3',
-  alinea: {
-    index: 'a0',
-    parent: entry2.id,
-    parents: [entry1.id, entry2.id],
-    workspace: 'main',
-    root: 'data'
-  }
+const entry3: FileInfo = {
+  modifiedAt: Date.now(),
+  workspace: 'main',
+  root: 'data',
+  filePath: 'index/entry2/entry3.json',
+  contents: encode({
+    id: 'entry3',
+    type: 'Type',
+    title: 'Entry 3',
+    path: '/entry3',
+    url: '/entry3',
+    alinea: {
+      index: 'a0',
+      parent: undefined,
+      parents: []
+    }
+  })
 }
 
-async function* entries() {
+const config = createConfig({
+  schema: schema({
+    Type: type('Type', {
+      title: text('Title'),
+      path: path('Path')
+    }).configure({
+      isContainer: true
+    })
+  }),
+  workspaces: {
+    main: workspace('Main', {
+      source: '.',
+      roots: {
+        data: root('Data', {
+          contains: ['Type']
+        })
+      }
+    })
+  }
+})
+
+async function* files() {
   yield* [entry1, entry2, entry3]
 }
 
 test('create', async () => {
   const {Database: SqlJsDb} = await sqlite()
-  const cnx = connect(new SqlJsDb())
-  const db = new Database(cnx.toAsync())
-  await db.fill(entries())
-  console.log(await db.state())
+  const cnx1 = connect(new SqlJsDb())
+  const db1 = new Database(cnx1.toAsync(), config)
+  await db1.fill(files())
+  console.log(await db1.meta())
+
+  const cnx2 = connect(new SqlJsDb())
+  const db2 = new Database(cnx2.toAsync(), config)
+  await db2.init()
+  await db2.syncWith(db1)
+
+  console.log(await db2.meta())
 })
 
 test.run()
--- src/core/util/ArrayBuffers.ts
+++ src/core/util/ArrayBuffers.ts
@@ -4,3 +4,14 @@ import {base16} from './Encoding.js'
 export function arrayBufferToHex(arrayBuffer: ArrayBuffer) {
   return base16.stringify(new Uint8Array(arrayBuffer), {pad: false})
 }
+
+export function concat(...arrays: Uint8Array[]) {
+  const totalLength = arrays.reduce((sum, array) => sum + array.length, 0)
+  const result = new Uint8Array(totalLength)
+  let offset = 0
+  for (const array of arrays) {
+    result.set(array, offset)
+    offset += array.length
+  }
+  return result
+}
