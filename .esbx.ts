908458873d391f6f22a98473b179ca66868e842a
Ben Merckx <ben@codeurs.be>
2023-06-21 08:47:47 +0200

    Add tag script

--- .esbx.ts
+++ .esbx.ts
@@ -1,5 +1,6 @@
 import {report} from '@esbx/util'
 import {getManifest} from '@esbx/workspaces'
+import {execSync} from 'child_process'
 import fs from 'fs-extra'
 
 export const VersionTask = {
@@ -9,5 +10,13 @@ export const VersionTask = {
     root.version = semver
     fs.writeFileSync('package.json', JSON.stringify(root, null, 2) + '\n')
     report(`bumped version to ${semver}`, false)
+  }
+}
+
+export const TagRelease = {
+  command: 'tag <semver>',
+  action(semver) {
+    execSync(`git tag -a v${semver} -m "v${semver}"`)
+    execSync(`git push --follow-tags`)
   }
 }
