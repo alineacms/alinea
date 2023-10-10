924efc80ec6f4beb9d36c1c9843b9049a53ecb53
Ben Merckx <ben@codeurs.be>
2023-10-10 10:12:12 +0200

    Attempt to make slugify unicode aware

--- src/core/util/Slugs.ts
+++ src/core/util/Slugs.ts
@@ -1,55 +1,15 @@
-const separate = new Set([
-  ' ',
-  '!',
-  '[',
-  ']',
-  ',',
-  '.',
-  ':',
-  '/',
-  '\\',
-  ';',
-  '<',
-  '>',
-  '?',
-  '_',
-  '-',
-  '|',
-  '(',
-  ')',
-  '#',
-  "'",
-  '’',
-  '`',
-  '´',
-  '"',
-  '“',
-  '”',
-  '*',
-  '{',
-  '}',
-  '\n',
-  '\r',
-  '\t'
-])
+// Anything that is not a letter, digit or emoji is considered a separator
+// A special case is made for punctuation which can be included in emojis,
+// which in this case would be followed by the emoji variation selector
+// For example we'll strip # but not #️⃣
+const strip = /([^\p{L}\p{N}\p{Emoji}\p{Emoji_Component}]|\p{P}(?!\u{fe0f}))+/gu
+const separator = '-'
+const trim = new RegExp(`^${separator}+|${separator}+$`, 'g')
 
-const space = ' '.charCodeAt(0)
-
-export function isSeparator(char: string) {
-  return separate.has(char)
+export function isSeparator(char: string): boolean {
+  return strip.test(char)
 }
 
-export function slugify(input: string, separator = '-'): string {
-  let buffer = ''
-  for (const char of input) {
-    if (isSeparator(char)) {
-      if (buffer === '') continue
-      if (!buffer.endsWith(separator)) buffer += separator
-      continue
-    }
-    if (char.charCodeAt(0) < space) continue
-    buffer += char
-  }
-  if (buffer.endsWith(separator)) buffer = buffer.slice(0, -separator.length)
-  return buffer.toLowerCase()
+export function slugify(input: string): string {
+  return input.replace(strip, separator).replace(trim, '').toLowerCase()
 }
