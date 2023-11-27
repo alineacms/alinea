f440593ca06db62460fb709d65a0d665094fbea6
Ben Merckx <ben@codeurs.be>
2023-11-27 16:58:12 +0100

    Remove outdated examples

--- examples/framework-next/schema/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './author.js'
-export * from './blog-container.js'
-export * from './blog-post.js'
-export * from './home-page.js'
--- examples/framework-remix/app/routes/cms/$.tsx
+++
@@ -1,4 +0,0 @@
-import {backend} from '@alinea/generated/backend'
-import {remixHandler} from 'alinea/backend/router/RemixHandler'
-
-export const loader = remixHandler(backend.handle)
