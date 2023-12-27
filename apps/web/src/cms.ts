48e3e0e4777f1e035578008c279a619dc679c649
Ben Merckx <ben@codeurs.be>
2023-12-27 12:36:00 +0100

    Undo config/cms split for web

--- apps/web/src/cms.ts
+++ apps/web/src/cms.ts
@@ -1,4 +1,37 @@
-import {createNextCMS} from 'alinea'
-import {config} from '../alinea.config'
+import alinea, {createNextCMS} from 'alinea'
+import * as schema from './schema'
+
+const pages = alinea.root('Pages', {
+  index: alinea.page(schema.Home),
+  roadmap: alinea.page(schema.Page),
+  docs: alinea.page(schema.Docs),
+  [alinea.meta]: {
+    contains: ['Page', 'Home']
+  }
+})
+
+const main = alinea.workspace('Alinea', {
+  pages,
+  media: alinea.media(),
+  [alinea.meta]: {
+    color: '#3F61E8',
+    mediaDir: 'public',
+    source: 'content'
+  }
+})
+
+const config = alinea.config({
+  schema,
+  workspaces: {main},
+  dashboard: {
+    dashboardUrl: '/admin.html',
+    handlerUrl: '/api/cms',
+    staticFile: 'public/admin.html'
+  },
+  preview:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/api/preview'
+      : '/api/preview'
+})
 
 export const cms = createNextCMS(config)
