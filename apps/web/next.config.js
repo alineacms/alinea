942055ea98c2b146d82acc34863dd49b3af799f5
Ben Merckx <ben@codeurs.be>
2023-12-28 10:55:19 +0100

    Revert alinea package as external for next.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     ignoreDuringBuilds: true
   },
   experimental: {
-    serverComponentsExternalPackages: ['alinea', '@alinea/generated']
+    serverComponentsExternalPackages: ['@alinea/generated']
   },
   rewrites() {
     return [
