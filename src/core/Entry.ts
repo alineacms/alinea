5a3354278804f7ec54f3cef0b8a656588d8b5aa0
Ben Merckx <ben@codeurs.be>
2024-01-09 12:16:25 +0100

    Define Entry type explicitly

--- src/core/Entry.ts
+++ src/core/Entry.ts
@@ -1,6 +1,38 @@
 import {Expand} from 'alinea/core'
-import {EntryRow} from './EntryRow.js'
+import {EntryPhase, EntryRow} from './EntryRow.js'
 import {Target} from './pages/Target.js'
 
-export type Entry = Expand<EntryRow>
+export interface Entry {
+  entryId: string
+  phase: EntryPhase
+  title: string
+  type: string
+  seeded: boolean
+  workspace: string
+  root: string
+  level: number
+  filePath: string
+  parentDir: string
+  childrenDir: string
+  index: string
+  parent: string | null
+  i18nId: string
+  locale: string | null
+  modifiedAt: number
+  rowHash: string
+  active: boolean
+  main: boolean
+  path: string
+  fileHash: string
+  url: string
+  data: Record<string, any>
+  searchableText: string
+}
 export const Entry = Target.create<Entry>({})
+
+// We can't export the inferred type because the rado depencency types are
+// not included in distribution. At least we'll get a type error here if the
+// type is out of date.
+type EntryFromRow = Expand<EntryRow>
+type Assert<A, B extends A> = never
+type Run = Assert<EntryFromRow, Entry> | Assert<Entry, EntryFromRow>
