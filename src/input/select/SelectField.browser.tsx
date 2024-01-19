8608e0ca876bcb38296e98cc105ed517425b0ca9
Ben Merckx <ben@codeurs.be>
2024-01-19 09:37:51 +0100

    Update select field

--- src/input/select/SelectField.browser.tsx
+++ src/input/select/SelectField.browser.tsx
@@ -1,11 +1,3 @@
-import {
-  autoUpdate,
-  flip,
-  offset,
-  size,
-  useFloating
-} from '@floating-ui/react-dom'
-import {Listbox} from '@headlessui/react'
 import {Field} from 'alinea/core'
 import {useField} from 'alinea/dashboard/editor/UseField'
 import {IconButton} from 'alinea/dashboard/view/IconButton'
@@ -15,6 +7,13 @@ import {IcRoundArrowDropDownCircle} from 'alinea/ui/icons/IcRoundArrowDropDownCi
 import {IcRoundCheck} from 'alinea/ui/icons/IcRoundCheck'
 import {IcRoundClose} from 'alinea/ui/icons/IcRoundClose'
 import {IcRoundUnfoldMore} from 'alinea/ui/icons/IcRoundUnfoldMore'
+import {
+  Button,
+  ListBox,
+  ListBoxItem,
+  Popover,
+  Select
+} from 'react-aria-components'
 import {SelectField, select as createSelect} from './SelectField.js'
 import css from './SelectInput.module.scss'
 
@@ -31,89 +30,59 @@ interface SelectInputProps<Key extends string> {
 function SelectInput<Key extends string>({field}: SelectInputProps<Key>) {
   const {value = null, mutator, label, options} = useField(field)
   const items = options.items as Record<string, string>
-  const {x, y, reference, floating, refs, strategy} = useFloating({
-    whileElementsMounted: autoUpdate,
-    strategy: 'fixed',
-    placement: 'bottom-start',
-    middleware: [
-      offset(4),
-      flip(),
-      size({
-        apply({rects}) {
-          if (refs.floating.current)
-            Object.assign(refs.floating.current.style, {
-              width: `${rects.reference.width}px`
-            })
-        }
-      })
-    ]
-  })
-
   return (
     <InputLabel {...options} icon={IcRoundArrowDropDownCircle}>
       <div className={styles.root()}>
-        <Listbox value={value} onChange={mutator} disabled={options.readOnly}>
-          {({open}) => (
-            <div>
-              <Listbox.Button
-                ref={reference}
-                className={styles.root.input({open})}
-              >
-                <span
-                  className={styles.root.input.label({placeholder: !value})}
-                >
-                  <TextLabel
-                    label={
-                      (value ? items[value] : options.placeholder) || label
-                    }
-                  />
-                </span>
-                <Icon
-                  icon={IcRoundUnfoldMore}
-                  className={styles.root.input.icon()}
+        <Select
+          aria-label={options.label}
+          selectedKey={value}
+          onSelectionChange={value => {
+            mutator(value as Key)
+          }}
+          isDisabled={options.readOnly}
+        >
+          <div>
+            <Button className={styles.root.input()}>
+              <span className={styles.root.input.label({placeholder: !value})}>
+                <TextLabel
+                  label={(value ? items[value] : options.placeholder) || label}
                 />
-                {value && options.optional && (
-                  <IconButton
-                    icon={IcRoundClose}
-                    onClick={() => mutator(undefined!)}
-                    className={styles.root.input.delete()}
-                  />
-                )}
-              </Listbox.Button>
-              <Listbox.Options
-                ref={floating}
-                style={{
-                  position: strategy,
-                  top: `${y || 0}px`,
-                  left: `${x || 0}px`
-                }}
-                className={styles.root.dropdown()}
-              >
-                <div className={styles.root.dropdown.inner()}>
-                  {Object.entries(items).map(([key, label]) => (
-                    <Listbox.Option key={key} value={key}>
-                      {({active, selected}) => (
-                        <HStack
-                          center
-                          gap={4}
-                          className={styles.root.dropdown.option({
-                            active,
-                            selected
-                          })}
-                        >
-                          <TextLabel label={label} />
-                          <div className={styles.root.dropdown.option.icon()}>
-                            {selected && <Icon size={18} icon={IcRoundCheck} />}
-                          </div>
-                        </HStack>
-                      )}
-                    </Listbox.Option>
-                  ))}
-                </div>
-              </Listbox.Options>
-            </div>
-          )}
-        </Listbox>
+              </span>
+              <Icon
+                icon={IcRoundUnfoldMore}
+                className={styles.root.input.icon()}
+              />
+              {value && options.optional && (
+                <IconButton
+                  icon={IcRoundClose}
+                  onClick={() => mutator(undefined!)}
+                  className={styles.root.input.delete()}
+                />
+              )}
+            </Button>
+            <Popover className={styles.root.dropdown()}>
+              <ListBox className={styles.root.dropdown.inner()}>
+                {Object.entries(items).map(([key, label]) => {
+                  const isSelected = key === value
+                  return (
+                    <ListBoxItem key={key} id={key}>
+                      <HStack
+                        center
+                        gap={4}
+                        className={styles.root.dropdown.option()}
+                      >
+                        <TextLabel label={label} />
+                        <div className={styles.root.dropdown.option.icon()}>
+                          {isSelected && <Icon size={18} icon={IcRoundCheck} />}
+                        </div>
+                      </HStack>
+                    </ListBoxItem>
+                  )
+                })}
+              </ListBox>
+            </Popover>
+          </div>
+        </Select>
       </div>
     </InputLabel>
   )
