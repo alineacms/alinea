d80c4245f4f13567c575528d4741989b45b52f9e
Ben Merckx <ben@codeurs.be>
2024-01-25 10:43:18 +0100

    Mention source maps in the docs

--- apps/web/src/schema/blocks/CodeBlock.ts
+++ apps/web/src/schema/blocks/CodeBlock.ts
@@ -13,7 +13,7 @@ export const CodeBlock = alinea.type('Code', {
     alinea.tab('Settings', {
       fileName: alinea.text('File name', {width: 0.75}),
       language: alinea.text('Language', {width: 0.25}),
-      compact: alinea.check('Compact', {label: 'Decrease line height'}),
+      compact: alinea.check('Compact', {description: 'Decrease line height'}),
       [alinea.tab.meta]: {
         icon: IcOutlineSettings
       }
