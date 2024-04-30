fd71dd4b65a5cba90f0cfb0b622f7818b0d2009e
Stijn Van Doorslaer <stijn@codeurs.be>
2024-04-30 10:10:48 +0200

    Added table icons (#383)

---
+++ src/ui/icons/TableDelete.tsx
@@ -0,0 +1,16 @@
+import {SVGProps} from 'react'
+
+export function TableDelete(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M19 4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h14ZM5 6v12h14V6H5Z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="m14.4 8.6 1.1 1-2.4 2.4 2.4 2.4-1.1 1.1-2.4-2.4-2.4 2.4-1-1.1 2.3-2.4-2.3-2.4 1-1 2.4 2.3z"
+      ></path>
+    </svg>
+  )
+}
---
+++ src/ui/icons/TableInsertRowBefore.tsx
@@ -0,0 +1,12 @@
+import {SVGProps} from 'react'
+
+export function TableInsertRowBefore(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M6 4a1 1 0 1 1 0 2H5v6h14V6h-1a1 1 0 0 1 0-2h2c.6 0 1 .4 1 1v13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-.6.4-1 1-1h2Zm5 10H5v4h6v-4Zm8 0h-6v4h6v-4ZM12 3c.5 0 1 .4 1 .9V6h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 .1V8H9a1 1 0 0 1 0-2h2V4c0-.6.4-1 1-1Z"
+      ></path>
+    </svg>
+  )
+}
