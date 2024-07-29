695ca6c9d54a4b0a460963498a729f3fe2634b9c
Ben Merckx <ben@codeurs.be>
2024-07-29 16:27:42 +0200

    Attempt to generalize driver

--- src/core/Driver.ts
+++ src/core/Driver.ts
@@ -1,7 +1,25 @@
+import {Request} from '@alinea/iso'
 import {Store} from 'alinea/backend/Store'
 import {Connection} from './Connection.js'
+import {PreviewRequest} from './Resolver.js'
+import {User} from './User.js'
 
 export interface Driver {
   createStore(cwd: string): Promise<Store>
   establishConnection(): Promise<Connection>
+}
+
+export interface DriverOptions {
+  createContext(request: Request): Promise<RequestContext>
+}
+
+export interface RequestContext {
+  inPreview?: boolean
+  user?: User
+  preview?: PreviewRequest
+  apiKey?: string
+}
+
+export class Driver {
+  constructor(options: DriverOptions) {}
 }
