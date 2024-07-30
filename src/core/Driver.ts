b9a44d356506ccb6aafb2a1758a8b24e66b42249
Ben Merckx <ben@codeurs.be>
2024-07-30 12:59:19 +0200

    Interim

--- src/core/Driver.ts
+++ src/core/Driver.ts
@@ -1,25 +1,53 @@
 import {Request} from '@alinea/iso'
-import {Store} from 'alinea/backend/Store'
-import {Connection} from './Connection.js'
+import {Database} from 'alinea/backend'
 import {PreviewRequest} from './Resolver.js'
 import {User} from './User.js'
 
-export interface Driver {
-  createStore(cwd: string): Promise<Store>
-  establishConnection(): Promise<Connection>
-}
-
-export interface DriverOptions {
-  createContext(request: Request): Promise<RequestContext>
-}
-
 export interface RequestContext {
-  inPreview?: boolean
-  user?: User
+  db: Database
+  auth?: {token: string; user: User}
   preview?: PreviewRequest
   apiKey?: string
 }
 
-export class Driver {
-  constructor(options: DriverOptions) {}
+export interface CreateContext {
+  (request: Request): Promise<RequestContext>
 }
+
+export class Driver {
+  #createContext: CreateContext
+  constructor(createContext: CreateContext) {
+    this.#createContext = createContext
+  }
+}
+
+/*
+const cms = createCMS(...)
+
+// next example
+// api/cms/route.ts
+import {handle} from 'alinea/next'
+export const GET = handle(cms)
+export const POST = handle(cms)
+
+// cloudflare pages example
+// functions/cms.ts
+import {handle} from 'alinea/cloudflare'
+export const onRequest = handle(cms)
+
+
+function createHandler<Input>(
+  getContext: (input: Input) => Promise<RequestContext>
+): (input: Input) => Promise<Response> {
+  return async input => {
+    const context = await getContext(input)
+    
+  }
+}
+
+const cloudHandler = createCloudHandler(this.config, db, process.env.ALINEA_API_KEY)
+function handle(cms, options) {
+
+}
+  
+*/
