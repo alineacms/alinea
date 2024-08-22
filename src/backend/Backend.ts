f7e2fef17bc2f19c4712207e4454a036980f5897
Ben Merckx <ben@codeurs.be>
2024-08-22 17:15:25 +0200

    Finish backend interface

--- src/backend/Backend.ts
+++ src/backend/Backend.ts
@@ -2,7 +2,6 @@ import {Connection} from 'alinea/core/Connection'
 import {Draft} from 'alinea/core/Draft'
 import {EntryRecord} from 'alinea/core/EntryRecord'
 import {Mutation} from 'alinea/core/Mutation'
-import {ResolveRequest} from 'alinea/core/Resolver'
 import {User} from 'alinea/core/User'
 import {Revision} from './History.js'
 
@@ -15,27 +14,48 @@ export interface AuthedContext extends RequestContext {
   token: string
 }
 
-export interface Backend {
-  resolve(ctx: RequestContext, params: ResolveRequest): Promise<unknown>
-  pendingSince(
-    ctx: RequestContext,
-    commitHash: string
-  ): Promise<{toCommitHash: string; mutations: Array<Mutation>} | undefined>
-  getDraft(ctx: RequestContext, entryId: string): Promise<Draft | undefined>
-  storeDraft(ctx: RequestContext, draft: Draft): Promise<void>
-
-  auth(ctx: RequestContext, request: Request): Promise<Response>
-  verify(ctx: RequestContext, request: Request): Promise<AuthedContext>
-
-  upload(ctx: AuthedContext, file: string): Promise<Connection.UploadResponse>
+export interface Target {
   mutate(
     ctx: AuthedContext,
     params: Connection.MutateParams
   ): Promise<{commitHash: string}>
-  listRevisions(ctx: AuthedContext, file: string): Promise<Array<Revision>>
-  getRevision(
+}
+
+export interface Auth {
+  authenticate(ctx: RequestContext, request: Request): Promise<Response>
+  verify(ctx: RequestContext, request: Request): Promise<AuthedContext>
+}
+
+export interface Media {
+  upload(ctx: AuthedContext, file: string): Promise<Connection.UploadResponse>
+}
+
+export interface Drafts {
+  get(ctx: AuthedContext, entryId: string): Promise<Draft | undefined>
+  store(ctx: AuthedContext, draft: Draft): Promise<void>
+}
+
+export interface History {
+  list(ctx: AuthedContext, file: string): Promise<Array<Revision>>
+  revision(
     ctx: AuthedContext,
     file: string,
     revisionId: string
   ): Promise<EntryRecord>
+}
+
+export interface Pending {
+  since(
+    ctx: RequestContext,
+    commitHash: string
+  ): Promise<{toCommitHash: string; mutations: Array<Mutation>} | undefined>
+}
+
+export interface Backend {
+  auth: Auth
+  target: Target
+  media: Media
+  drafts: Drafts
+  history: History
+  pending: Pending
 }
