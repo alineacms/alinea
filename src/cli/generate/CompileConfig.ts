e95f72d78334031755a776ff29b82b774dc1a061
Ben Merckx <ben@codeurs.be>
2024-09-19 13:42:10 +0200

    Remove workaround for evanw/esbuild#2460

--- src/cli/generate/CompileConfig.ts
+++ src/cli/generate/CompileConfig.ts
@@ -7,35 +7,13 @@ import {ignorePlugin} from '../util/IgnorePlugin.js'
 import {publicDefines} from '../util/PublicDefines.js'
 import {GenerateContext} from './GenerateContext.js'
 
-// Workaround evanw/esbuild#2460
-function overrideTsConfig(cwd: string): string | undefined {
-  const overrideLocation = path.join(cwd, 'tsconfig.alinea.json')
-  // Did we already extend?
-  if (fs.existsSync(overrideLocation)) return overrideLocation
-  // Do we have an existing tsconfig to extend?
-  const tsConfig = path.join(cwd, 'tsconfig.json')
-  const hasTsConfig = fs.existsSync(tsConfig)
-  if (hasTsConfig) {
-    // Unfortunately the only way to overwrite the jsx setting is to provide
-    // esbuild with a path to another tsconfig file within the same dir
-    // We used to check the jsx value here before, but it requires a bunch of
-    // dependencies to read it correctly, so always overriding is much easier
-    const extendedConfig = {
-      extends: './tsconfig.json',
-      compilerOptions: {jsx: 'react-jsx'}
-    }
-    fs.writeFileSync(overrideLocation, JSON.stringify(extendedConfig, null, 2))
-    return overrideLocation
-  }
-}
-
 export function compileConfig({
   rootDir,
   outDir,
   configLocation,
   watch
 }: GenerateContext) {
-  const tsConfigFile = overrideTsConfig(rootDir)
+  const tsConfigFile = path.join(rootDir, 'tsconfig.json')
   const define = publicDefines(process.env)
   const results = createEmitter<BuildResult>()
   const config: BuildOptions = {
@@ -74,7 +52,7 @@ export function compileConfig({
         }
       }
     ],
-    tsconfig: tsConfigFile
+    tsconfig: fs.existsSync(tsConfigFile) ? tsConfigFile : undefined
   }
   if (watch) {
     esbuild.context(config).then(context => context.watch())
