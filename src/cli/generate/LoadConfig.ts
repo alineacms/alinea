2ab944d9816bc5b1a8162385a63c0f20ac82bf22
Ben Merckx <ben@codeurs.be>
2024-09-20 11:13:16 +0200

    Minor

--- src/cli/generate/LoadConfig.ts
+++ src/cli/generate/LoadConfig.ts
@@ -2,14 +2,15 @@ import {createCMS} from 'alinea/core'
 import {CMS} from 'alinea/core/CMS'
 import {createRequire} from 'node:module'
 import path from 'node:path'
+import {pathToFileURL} from 'node:url'
 
 export async function loadCMS(outDir: string): Promise<CMS> {
   const unique = Date.now()
-  const genConfigFile = path.join(outDir, 'config.js').replace(/\\/g, '/')
+  const genConfigFile = path.join(outDir, 'config.js')
   // Passing a unique identifier makes sure we don't receive the same module
   // from the registry. Unfortunately this also means each config module will
   // remain in memory.
-  const outFile = `file://${genConfigFile}?${unique}`
+  const outFile = `${pathToFileURL(genConfigFile)}?${unique}`
   global.require = createRequire(import.meta.url)
   const exports = await import(outFile)
   if ('cms' in exports && exports.cms instanceof CMS) return exports.cms
