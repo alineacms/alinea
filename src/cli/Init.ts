7140a0ccc6246fb496ec3b222754c32b7347c682
Ben Merckx <ben@codeurs.be>
2024-10-28 09:14:40 +0530

    Detect Next.js for init cmd. This should really just be a selection prompt which we'll get to.

--- src/cli/Init.ts
+++ src/cli/Init.ts
@@ -64,27 +64,26 @@ export async function init(options: InitOptions) {
     path.join(__dirname, 'static/init/cms.js'),
     'utf-8'
   )
+  const pm = await detectPm()
+  const runner = pm === 'npm' ? 'npx' : pm
+  let [pkg] = await outcome(
+    fs.readFile(path.join(cwd, 'package.json'), 'utf-8')
+  )
+  if (pkg) {
+    pkg = pkg.replace('"dev": "', '"dev": "alinea dev -- ')
+    pkg = pkg.replace('"build": "', '"build": "alinea build -- ')
+    await fs.writeFile(path.join(cwd, 'package.json'), pkg)
+    try {
+      const isNext = JSON.parse(pkg).dependencies?.next
+      if (isNext) options.next = true
+    } catch {}
+  }
   const configFileContents = options.next
     ? configFile.replaceAll('alinea/core', 'alinea/next')
     : configFile
   const hasSrcDir = (await outcome(fs.stat(path.join(cwd, 'src')))).isSuccess()
-  const configFileLocation = path.join(
-    cwd,
-    hasSrcDir ? 'src/cms.tsx' : 'cms.tsx'
-  )
+  const configFileLocation = path.join(cwd, hasSrcDir ? 'src/cms.ts' : 'cms.ts')
   await fs.writeFile(configFileLocation, configFileContents)
-  const pm = await detectPm()
-  if (options.next) {
-    let [pkg] = await outcome(
-      fs.readFile(path.join(cwd, 'package.json'), 'utf-8')
-    )
-    if (pkg) {
-      pkg = pkg.replace('"dev": "', '"dev": "alinea dev -- ')
-      pkg = pkg.replace('"build": "', '"build": "alinea build -- ')
-      await fs.writeFile(path.join(cwd, 'package.json'), pkg)
-    }
-  }
-  const runner = pm === 'npm' ? 'npx' : pm
   const command = `${runner} alinea dev`
   if (!quiet)
     console.info(
