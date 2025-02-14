27ef4f437884e00b9454ca3d1643b526d277ff46
Ben Merckx <ben@codeurs.be>
2025-02-14 13:44:54 +0100

    Actual status might be different if a parent is archived

--- src/core/CMS.ts
+++ src/core/CMS.ts
@@ -64,7 +64,7 @@ export class CMS<Definition extends Config = Config> extends Graph {
       type: query.type,
       id: op.id,
       locale: query.locale,
-      status: query.status
+      status: 'preferPublished'
     })
   }
 
