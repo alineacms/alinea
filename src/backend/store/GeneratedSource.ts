fc97774475f0bdd22ce988b30ca8dd94ab2b55c2
Ben Merckx <ben@codeurs.be>
2025-04-07 12:12:21 +0200

    Avoid ts errors if source exists

--- src/backend/store/GeneratedSource.ts
+++ src/backend/store/GeneratedSource.ts
@@ -3,8 +3,13 @@ import {importSource} from 'alinea/core/source/SourceExport'
 import PLazy from 'p-lazy'
 
 export const generatedSource: Promise<MemorySource> = PLazy.from(async () => {
-  // @ts-ignore
-  return import('@alinea/generated/source.json', {with: {type: 'json'}})
-    .then(imported => importSource(imported))
-    .catch(() => new MemorySource())
+  try {
+    // @ts-ignore
+    const source: any = await import('@alinea/generated/source.json', {
+      with: {type: 'json'}
+    })
+    return await importSource(source)
+  } catch {
+    return new MemorySource()
+  }
 })
