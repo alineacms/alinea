e5a3411fab7f1d5400ac65335b365f194a889360
Ben Merckx <ben@codeurs.be>
2025-04-24 10:54:17 +0200

    Optimize link resolver a little

--- src/core/db/LinkResolver.ts
+++ src/core/db/LinkResolver.ts
@@ -1,5 +1,10 @@
 import type {InferProjection, Projection} from 'alinea/core/Graph'
-import type {EntryResolver, ResolveContext} from './EntryResolver.js'
+import type {Entry} from '../Entry.js'
+import {
+  type EntryResolver,
+  type ResolveContext,
+  statusChecker
+} from './EntryResolver.js'
 
 export class LinkResolver {
   constructor(
@@ -7,14 +12,27 @@ export class LinkResolver {
     private ctx: ResolveContext
   ) {}
 
-  resolveLinks<P extends Projection>(
+  async resolveLinks<P extends Projection>(
     projection: P,
     entryIds: ReadonlyArray<string>
   ): Promise<Array<InferProjection<P> | undefined>> {
-    return this.resolver.resolve({
-      select: projection,
-      status: this.ctx.status,
-      id: {in: entryIds}
-    }) as any
+    const conditionStatus = statusChecker(this.ctx.status)
+    const entries = Array<Entry>()
+    for (const id of entryIds) {
+      const node = this.resolver.index.byId.get(id)
+      if (!node) continue
+      const locale = node.locales.has(null) ? null : this.ctx.locale
+      const versions = node.locales.get(locale ?? null)
+      if (!versions) continue
+      for (const version of versions.values()) {
+        if (conditionStatus(version)) entries.push(version)
+      }
+    }
+    const q = {select: projection}
+    const results = entries.map((entry): any => {
+      return this.resolver.select(this.ctx, entry, q)
+    })
+    await this.resolver.post({linkResolver: this}, results, q as any)
+    return results
   }
 }
