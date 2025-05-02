955fdb6c5ccb7035ec1fddc24f1f614428c8a5da
Ben Merckx <ben@codeurs.be>
2025-05-02 11:21:15 +0200

    Remove browser condition for this file as it is not used outside of the dashboard (anymore)

--- src/dashboard/view/Preview.tsx
+++ src/dashboard/view/Preview.tsx
@@ -1,7 +1,16 @@
+import styler from '@alinea/styler'
 import type {PropsWithChildren} from 'react'
+import css from './Preview.module.scss'
+import {Sidebar} from './Sidebar.js'
+
+const styles = styler(css)
 
 export type PreviewProps = PropsWithChildren<{}>
 
 export function Preview({children}: PreviewProps) {
-  return null
+  return (
+    <Sidebar.Preview>
+      <div className={styles.root()}>{children}</div>
+    </Sidebar.Preview>
+  )
 }
