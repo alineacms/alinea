f4a5339c398e4dd680f1b8d087b4e499430f5066
Stijn Van Doorslaer <stijn@codeurs.be>
2025-07-10 12:01:13 +0200

    Implemented getMetadata function

--- apps/web/src/schema/BlogOverview.tsx
+++ apps/web/src/schema/BlogOverview.tsx
@@ -1,9 +1,6 @@
-import {Config, Field} from 'alinea'
+import {Config} from 'alinea'
 
-export const BlogOverview = Config.type('Blog overview', {
+export const BlogOverview = Config.document('Blog overview', {
   contains: ['BlogPost'],
-  fields: {
-    title: Field.text('Title', {width: 0.5}),
-    path: Field.path('Path', {width: 0.5})
-  }
+  fields: {}
 })
