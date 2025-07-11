7b357fa43768cbd594668cf19b6f458f92f17000
Stijn Van Doorslaer <stijn@codeurs.be>
2025-07-11 15:45:00 +0200

    Improve accessibility of list field buttons (#429)
    * ListField added titles for accessibility
    
    * IconButton added cursor pointer
    
    * EntryHeader menu and preview toggle layout

---
+++ src/ui/icons/IcRoundLastPage.tsx
@@ -0,0 +1,18 @@
+import type {SVGProps} from 'react'
+
+export function IcRoundLastPage(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M6.29 8.11L10.18 12l-3.89 3.89A.996.996 0 1 0 7.7 17.3l4.59-4.59a.996.996 0 0 0 0-1.41L7.7 6.7a.996.996 0 0 0-1.41 0c-.38.39-.38 1.03 0 1.41M17 6c.55 0 1 .45 1 1v10c0 .55-.45 1-1 1s-1-.45-1-1V7c0-.55.45-1 1-1"
+      />
+    </svg>
+  )
+}
