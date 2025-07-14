f05875be8b971427b6c9a55cef289fe2977b176c
Stijn Van Doorslaer <stijn@codeurs.be>
2025-07-14 12:26:36 +0200

    Playground metadata changes

--- apps/web/src/page/PlaygroundPage.tsx
+++ apps/web/src/page/PlaygroundPage.tsx
@@ -1,17 +1,24 @@
 import {promises as fs} from 'node:fs'
 import {Loader} from 'alinea/ui'
-import type {MetadataRoute} from 'next'
+import type {Metadata, MetadataRoute, Viewport} from 'next'
 import {Suspense} from 'react'
+import {getMetadata, type MetadataProps} from '@/utils/metadata'
 import {PlaygroundDynamic} from './playground/Playground.dynamic'
 
-export const metadata = {
-  title: 'Alinea CMS playground'
+export const viewport: Viewport = {
+  themeColor: '#3f61e8'
 }
 
-export const viewport = {
-  themeColor: '#4a65e8'
+export async function generateMetadata(): Promise<Metadata> {
+  return await getMetadata({
+    url: '/playground',
+    title: 'Playground',
+    metadata: {
+      description:
+        'Try out different field types and validation rules. Experiment, test, and preview field configurations in real-time.'
+    }
+  } as MetadataProps)
 }
-
 export default async function PlaygroundPage() {
   const declarations = await fs.readFile(
     `${process.cwd()}/src/page/playground/alinea.d.ts.txt`,
