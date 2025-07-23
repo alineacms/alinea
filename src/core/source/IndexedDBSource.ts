f8f74d937f5358d2cfde90ac1f53a66bea3c7618
Ben Merckx <ben@codeurs.be>
2025-07-23 15:06:34 +0200

    Attempt to work around "Failed to execute 'transaction' on 'IDBDatabase': The database connection is closing"

--- src/core/source/IndexedDBSource.ts
+++ src/core/source/IndexedDBSource.ts
@@ -3,12 +3,26 @@ import type {Source} from './Source.js'
 import {ReadonlyTree} from './Tree.js'
 
 export class IndexedDBSource implements Source {
-  #db: Promise<IDBDatabase>
+  #factory: IDBFactory
+  #name: string
+  #connection: Promise<IDBDatabase> | undefined
 
   constructor(indexedDB: IDBFactory, name: string) {
-    this.#db = new Promise<IDBDatabase>((resolve, reject) => {
-      const request = indexedDB.open(name)
-      request.onsuccess = () => resolve(request.result)
+    this.#factory = indexedDB
+    this.#name = name
+  }
+
+  #createConnection(): Promise<IDBDatabase> {
+    return new Promise<IDBDatabase>((resolve, reject) => {
+      const request = this.#factory.open(this.#name)
+      request.onsuccess = () => {
+        const db = request.result
+        db.onclose = () => {
+          console.info('IndexedDB connection closed')
+          this.#connection = undefined
+        }
+        resolve(db)
+      }
       request.onerror = () => reject(request.error)
       request.onupgradeneeded = () => {
         const db = request.result
@@ -18,18 +32,37 @@ export class IndexedDBSource implements Source {
     })
   }
 
-  async getTree() {
-    const db = await this.#db
-    return new Promise<ReadonlyTree>((resolve, reject) => {
-      const transaction = db.transaction(['tree'], 'readonly')
-      const store = transaction.objectStore('tree')
-      const request = store.get('tree')
-      request.onsuccess = event => {
-        const entry = (event.target as IDBRequest).result
-        resolve(entry ? new ReadonlyTree(entry) : ReadonlyTree.EMPTY)
+  #connect(): Promise<IDBDatabase> {
+    this.#connection ??= this.#createConnection()
+    return this.#connection
+  }
+
+  #retryIfClosing<T extends Function>(handle: T) {
+    return (error: Error) => {
+      if (error instanceof Error && error.message.includes('closing')) {
+        // If the database is closing, we retry the operation handle
+        this.#connection = undefined
+        return handle()
       }
-      request.onerror = event => reject((event.target as IDBRequest).error)
-    })
+      throw error
+    }
+  }
+
+  getTree(): Promise<ReadonlyTree> {
+    const handle = async () => {
+      const db = await this.#connect()
+      return new Promise<ReadonlyTree>((resolve, reject) => {
+        const transaction = db.transaction(['tree'], 'readonly')
+        const store = transaction.objectStore('tree')
+        const request = store.get('tree')
+        request.onsuccess = event => {
+          const entry = (event.target as IDBRequest).result
+          resolve(entry ? new ReadonlyTree(entry) : ReadonlyTree.EMPTY)
+        }
+        request.onerror = event => reject((event.target as IDBRequest).error)
+      })
+    }
+    return handle().catch(this.#retryIfClosing(handle))
   }
 
   async getTreeIfDifferent(sha: string): Promise<ReadonlyTree | undefined> {
@@ -40,7 +73,7 @@ export class IndexedDBSource implements Source {
   async *getBlobs(
     shas: Array<string>
   ): AsyncGenerator<[sha: string, blob: Uint8Array]> {
-    const db = await this.#db
+    const db = await this.#connect()
     const transaction = db.transaction(['blobs'], 'readonly')
     const store = transaction.objectStore('blobs')
     for (const sha of shas) {
@@ -57,7 +90,7 @@ export class IndexedDBSource implements Source {
   }
 
   async applyChanges(batch: ChangesBatch) {
-    const db = await this.#db
+    const db = await this.#connect()
     const current = await this.getTree()
     const updatedTree = current.clone()
     updatedTree.applyChanges(batch)
