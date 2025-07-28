c556767fec6425558265e66ac52f3e043077d9b9
Brecht Cloetens <brecht@codeurs.be>
2025-07-28 13:56:53 +0200

    Changed formatting

--- src/field/list/ListField.view.tsx
+++ src/field/list/ListField.view.tsx
@@ -1,32 +1,32 @@
 import styler from '@alinea/styler'
 import {
+  closestCenter,
   DndContext,
   type DragEndEvent,
+  type DraggableSyntheticListeners,
   DragOverlay,
   type DragStartEvent,
-  type DraggableSyntheticListeners,
+  defaultDropAnimation,
   KeyboardSensor,
   LayoutMeasuringStrategy,
   PointerSensor,
-  closestCenter,
-  defaultDropAnimation,
   useSensor,
   useSensors
 } from '@dnd-kit/core'
 import {
   type AnimateLayoutChanges,
-  SortableContext,
   defaultAnimateLayoutChanges,
+  SortableContext,
   sortableKeyboardCoordinates,
   useSortable,
   verticalListSortingStrategy
 } from '@dnd-kit/sortable'
 import {CSS, type FirstArgument} from '@dnd-kit/utilities'
+import type {ListField} from 'alinea/core/field/ListField'
 import {getType} from 'alinea/core/Internal'
 import type {Schema} from 'alinea/core/Schema'
-import {Type} from 'alinea/core/Type'
-import type {ListField} from 'alinea/core/field/ListField'
 import {ListRow} from 'alinea/core/shape/ListShape'
+import {Type} from 'alinea/core/Type'
 import {entries} from 'alinea/core/util/Objects'
 import {FormRow} from 'alinea/dashboard/atoms/FormAtoms'
 import {InputForm} from 'alinea/dashboard/editor/InputForm'
@@ -35,7 +35,6 @@ import {Create} from 'alinea/dashboard/view/Create'
 import {IconButton} from 'alinea/dashboard/view/IconButton'
 import {InputLabel} from 'alinea/dashboard/view/InputLabel'
 import {Icon, TextLabel} from 'alinea/ui'
-import {Sink} from 'alinea/ui/Sink'
 import {IcBaselineContentCopy} from 'alinea/ui/icons/IcBaselineContentCopy'
 import {IcBaselineContentPasteGo} from 'alinea/ui/icons/IcBaselineContentPasteGo'
 import {IcOutlineList} from 'alinea/ui/icons/IcOutlineList'
@@ -44,6 +43,7 @@ import {IcRoundClose} from 'alinea/ui/icons/IcRoundClose'
 import {IcRoundDragHandle} from 'alinea/ui/icons/IcRoundDragHandle'
 import {IcRoundKeyboardArrowDown} from 'alinea/ui/icons/IcRoundKeyboardArrowDown'
 import {IcRoundKeyboardArrowUp} from 'alinea/ui/icons/IcRoundKeyboardArrowUp'
+import {Sink} from 'alinea/ui/Sink'
 import {useAtom} from 'jotai'
 import {atomWithStorage} from 'jotai/utils'
 import {
@@ -171,7 +171,10 @@ function ListInputRow({
           />
         </Sink.Options>
         <Sink.Title>
-          <TextLabel label={Type.label(type)} className={styles.row.header.title()} />
+          <TextLabel
+            label={Type.label(type)}
+            className={styles.row.header.title()}
+          />
         </Sink.Title>
         <Sink.Options>
           {onCopyBlock !== undefined && (
@@ -193,7 +196,11 @@ function ListInputRow({
                 onClick={() => onMove?.(1)}
                 title="Move down one position"
               />
-              <IconButton icon={IcRoundClose} onClick={onDelete} title="Delete block" />
+              <IconButton
+                icon={IcRoundClose}
+                onClick={onDelete}
+                title="Delete block"
+              />
             </>
           )}
         </Sink.Options>
@@ -261,7 +268,11 @@ function ListInsertRow({first, open, onInsert}: ListInsertRowProps) {
   return (
     <>
       <div className={styles.insert({open, first})}>
-        <button className={styles.insert.icon()} onClick={onInsert} title="Insert new block">
+        <button
+          className={styles.insert.icon()}
+          onClick={onInsert}
+          title="Insert new block"
+        >
           <Icon icon={open ? IcRoundKeyboardArrowUp : IcRoundAdd} />
         </button>
       </div>
