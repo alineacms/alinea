b561aac2c9861ec8dac296062765739062d3fc05
Ben Merckx <ben@codeurs.be>
2025-07-31 11:10:33 +0200

    Unwrap these correctly

--- src/cloud/CloudRemote.ts
+++ src/cloud/CloudRemote.ts
@@ -293,9 +293,14 @@ async function parseOutcome<T>(expected: Promise<Response>): Promise<T> {
   }
   const output = await response.json()
   if (output.success) {
-    return output.value as T
+    return output.data as T
   }
   if (output.error) {
+    if (typeof output.error === 'object') {
+      const error = new HttpError(response.status, 'Unexpected error')
+      Object.assign(error, output.error)
+      throw error
+    }
     throw new HttpError(response.status, output.error)
   }
   throw new HttpError(
