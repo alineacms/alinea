c5ed7d19da75e893a2fe29a54bd89f8759b0c691
Ben Merckx <ben@codeurs.be>
2025-08-01 10:59:07 +0200

    Retain more information for auth failures

--- src/backend/Auth.ts
+++ src/backend/Auth.ts
@@ -1,6 +1,17 @@
+import {HttpError} from 'alinea/core/HttpError.js'
+
 export enum AuthAction {
   Status = 'status',
   Handshake = 'handshake',
   Login = 'login',
   Logout = 'logout'
 }
+
+export class AuthError extends HttpError {
+  constructor(message: string, options?: ErrorOptions) {
+    super(401, message, options)
+  }
+}
+
+export class MissingCredentialsError extends AuthError {}
+export class InvalidCredentialsError extends AuthError {}
