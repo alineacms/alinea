f6a1914ffa0b1c310e1823350f12098eb4f08f53
David Merckx <david@codeurs.be>
2025-11-03 12:26:55 +0000

    Error page

---
+++ apps/demo/src/app/error.tsx
@@ -0,0 +1,31 @@
+'use client' // Error boundaries must be Client Components
+
+import {useEffect} from 'react'
+
+export default function ErrorPage({
+  error,
+  reset
+}: {
+  error: Error & {digest?: string}
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        type="button"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
