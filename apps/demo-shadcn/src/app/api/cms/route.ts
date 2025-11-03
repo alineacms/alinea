992d39b167f7118292d854f44f71a7ef56aef99e
David Merckx <david@codeurs.be>
2025-11-03 12:44:08 +0000

    Fix demo build

--- apps/demo-shadcn/src/app/api/cms/route.ts
+++
@@ -1,8 +0,0 @@
-//import {db} from '@vercel/postgres'
-import {createHandler} from 'alinea/next'
-import {cms} from '@/cms'
-
-const handler = createHandler({cms})
-
-export const GET = handler
-export const POST = handler
