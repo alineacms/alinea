a958f58d4bc54e35ab59b26ab1d127d9fb8577fa
Stijn Van Doorslaer <stijn@codeurs.be>
2025-11-28 14:50:18 +0100

    Richtext table (#462)
    * EntryHeader previewToggle switched icon
    
    * RichTextToolbar table extensions

---
+++ src/ui/icons/TableMergeCells.tsx
@@ -0,0 +1,12 @@
+import type {SVGProps} from 'react'
+
+export function TableMergeCells(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M19 4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h14ZM5 15.5V18h3v-2.5H5Zm14-5h-9V18h9v-7.5ZM19 6h-4v2.5h4V6ZM8 6H5v2.5h3V6Zm5 0h-3v2.5h3V6Zm-8 7.5h3v-3H5v3Z"
+      />
+    </svg>
+  )
+}
