8d61d78dc15f2c7c234a583f106e405d507bf777
Ben Merckx <ben@codeurs.be>
2026-01-15 14:45:49 +0100

    Fix search results

--- src/core/db/EntryIndex.ts
+++ src/core/db/EntryIndex.ts
@@ -323,7 +323,9 @@ export class EntryGraph {
         return text
           .normalize('NFD')
           .replace(DIACRITIC, '')
+          .toLowerCase()
           .split(SPACE_OR_PUNCTUATION)
+          .filter(Boolean)
       }
     })
     for (const [file, version] of versionData) {
@@ -372,7 +374,7 @@ export class EntryGraph {
       for (const entry of found) {
         if (!this.#search.has(entry.filePath)) this.#updateSearch(entry)
       }
-      return this.#search
+      const results = this.#search
         .search(search, {
           prefix: true,
           fuzzy: 0.1,
@@ -380,6 +382,8 @@ export class EntryGraph {
           filter: result => found.has(result.entry)
         })
         .map(result => result.entry)
+      yield* results
+      return
     }
     for (const node of filter.nodes ?? this.nodes) {
       if (filter.node && !filter.node(node)) continue
