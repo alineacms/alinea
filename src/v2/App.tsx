e86b4a1e485cac0c245a258e363024b56aacd9cd
Ben Merckx <ben@codeurs.be>
2026-02-24 16:21:26 +0100

    Setup a v2 folder

---
+++ src/v2/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+export function App() {
+  return (
+    <div className="app">
+      <h1>Bun + React</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test
+      </p>
+    </div>
+  )
+}
+
+export default App
