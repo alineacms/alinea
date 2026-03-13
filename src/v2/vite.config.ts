8941d4c65afa53e0b696eef2f5db00087698e0cb
Ben Merckx <ben@codeurs.be>
2026-03-13 13:13:10 +0100

    Update vite

--- src/v2/vite.config.ts
+++ src/v2/vite.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import {alineaFixturePlugin} from './plugins/alineaFixturePlugin'
 
 export default defineConfig({
-  plugins: [alineaFixturePlugin(), tsconfigPaths(), react()]
+  plugins: [alineaFixturePlugin(), react()],
+  resolve: {
+    tsconfigPaths: true
+  }
 })
