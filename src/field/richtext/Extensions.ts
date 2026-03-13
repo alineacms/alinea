d2ddb13b5ee9def529010bb76ece92da9ce365d7
Stijn Van Doorslaer <stijn@codeurs.be>
2026-03-13 11:05:57 +0100

    RichText extensions added highlight

--- src/field/richtext/Extensions.ts
+++ src/field/richtext/Extensions.ts
@@ -8,6 +8,7 @@ import FloatingMenu from '@tiptap/extension-floating-menu'
 import Gapcursor from '@tiptap/extension-gapcursor'
 import HardBreak from '@tiptap/extension-hard-break'
 import Heading from '@tiptap/extension-heading'
+import Highlight from '@tiptap/extension-highlight'
 import HorizontalRule from '@tiptap/extension-horizontal-rule'
 import Italic from '@tiptap/extension-italic'
 import ListItem from '@tiptap/extension-list-item'
@@ -99,5 +100,10 @@ export const extensions = {
       class: styles.th()
     }
   }),
-  TableRow
+  TableRow,
+  Highlight: Highlight.configure({
+    HTMLAttributes: {
+      class: styles.highlight()
+    }
+  })
 }
