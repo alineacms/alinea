164f0476c837492b51feba5c424217c68ad7bd65
Ben Merckx <ben@codeurs.be>
2026-03-26 15:05:32 +0100

    Interim

--- src/v2/app/fields/RichTextField.view.tsx
+++ src/v2/app/fields/RichTextField.view.tsx
@@ -9,12 +9,16 @@ import {
   ToolbarSeparator
 } from '@alinea/components'
 import styler from '@alinea/styler'
+import {EditorContent, JSONContent, useEditor} from '@tiptap/react'
 import {RichTextField as CoreRichTextField} from 'alinea/core/field/RichTextField'
 import {Schema} from 'alinea/core/Schema'
+import {BlockNode, ElementNode, Mark, Node, TextNode} from 'alinea/core/TextDoc'
+import {entries, values} from 'alinea/core/util/Objects'
+import {extensions as baseExtensions} from 'alinea/field/richtext/Extensions'
 import {RichTextOptions} from 'alinea/field/richtext/RichTextField'
 import {IcRoundUnfoldMore} from 'alinea/ui/icons/IcRoundUnfoldMore'
-import {useFieldOptions} from 'alinea/v2/store'
-import {memo, useMemo} from 'react'
+import {useFieldOptions, useFieldValue} from 'alinea/v2/store'
+import {memo} from 'react'
 import {createPortal} from 'react-dom'
 import css from './RichTextField.module.css'
 
@@ -28,7 +32,14 @@ export const RichTextFieldView = memo(function RichTextFieldView<
   Blocks extends Schema
 >({field}: RichTextFieldViewProps<Blocks>) {
   const options = useFieldOptions(field)
-  const toolbar = useMemo(() => document.getElementById('alinea-toolbar'), [])
+  const toolbar = document.getElementById('alinea-toolbar')
+  const [value, setValue] = useFieldValue(field)
+  const content = value?.map(toContent)
+  console.log('content', content)
+  const editor = useEditor({
+    content: content,
+    extensions: values(baseExtensions)
+  })
   return (
     <>
       <Label
@@ -36,7 +47,7 @@ export const RichTextFieldView = memo(function RichTextFieldView<
         isRequired={options.required}
         label={options.label}
       >
-        here comes text
+        <EditorContent editor={editor} />
       </Label>
       {toolbar && createPortal(<RichTextToolbar />, toolbar)}
     </>
@@ -105,4 +116,31 @@ function RichTextToolbar() {
       </ToolbarGroup>
     </Toolbar>
   )
+}
+
+function toContent(node: Node): JSONContent {
+  if (Node.isText(node))
+    return {
+      type: 'text',
+      text: node[TextNode.text],
+      marks: node[TextNode.marks]?.map(mark => {
+        const {[Mark.type]: type, ...attrs} = mark
+        const res = Object.fromEntries(
+          entries(attrs).map(([key, value]) => {
+            if (key.startsWith('_')) return [`data-${key.slice(1)}`, value]
+            return [key, value]
+          })
+        )
+        return {type, attrs: res}
+      })
+    }
+  if (Node.isElement(node)) {
+    const {[Node.type]: type, [ElementNode.content]: content, ...attrs} = node
+    return {type, content: content?.map(toContent), attrs}
+  }
+  if (Node.isBlock(node)) {
+    const {[Node.type]: type} = node
+    return {type, attrs: {[BlockNode.id]: node[BlockNode.id]}}
+  }
+  throw new TypeError('Invalid node')
 }
