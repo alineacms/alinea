61799984e541e307b28cd558e6b8538792e87255
Ben Merckx <ben@codeurs.be>
2026-05-06 09:55:52 +0200

    Port path field

--- src/dashboard/app/field/path/PathField.view.tsx
+++ src/dashboard/app/field/path/PathField.view.tsx
@@ -1,24 +1,93 @@
-import {TextField as RacTextField} from '#/components.js'
-import {PathField} from '#/field/path.js'
-import {memo} from 'react'
-import {useField, useFieldError, useFieldOptions} from '../../../store.js'
+import {TextField} from '#/components.js'
+import {isSeparator, slugify} from '#/core/util/Slugs.js'
+import {IcRoundLink} from '#/dashboard/icons.js'
+import {
+  useField,
+  useFieldError,
+  useFieldOptions,
+  useSiblingFieldValue
+} from '#/dashboard/store.js'
+import type {PathField} from '#/field/path.js'
+import type {ReactNode} from 'react'
+import {memo, useState} from 'react'
 
-export interface TextInputProps {
+export interface PathFieldViewProps {
   field: PathField
 }
 
 export const PathFieldView = memo(function PathFieldView({
   field
-}: TextInputProps) {
-  const [value = '', setValue] = useField(field)
+}: PathFieldViewProps) {
+  const [fieldValue, setValue] = useField(field)
   const options = useFieldOptions(field)
   const error = useFieldError(field)
+  const sourceKey = options.from ?? 'title'
+  const sourceValue = useSiblingFieldValue(sourceKey)
+  const source = typeof sourceValue === 'string' ? sourceValue : ''
+
   return (
-    <RacTextField
-      label={options.label}
-      value={value}
-      onChange={setValue}
+    <PathInput
+      description={options.help}
+      errorMessage={error}
+      fieldValue={fieldValue}
       isInvalid={Boolean(error)}
+      isReadOnly={options.readOnly}
+      isRequired={options.required}
+      label={options.label}
+      source={source}
+      onChange={setValue}
     />
   )
 })
+
+export interface PathInputProps {
+  description?: ReactNode
+  errorMessage?: ReactNode
+  fieldValue?: string
+  isInvalid?: boolean
+  isReadOnly?: boolean
+  isRequired?: boolean
+  label?: ReactNode
+  onChange: (value: string) => void
+  source?: string
+}
+
+export function PathInput({
+  description,
+  errorMessage,
+  fieldValue,
+  isInvalid,
+  isReadOnly,
+  isRequired,
+  label,
+  onChange,
+  source = ''
+}: PathInputProps) {
+  const [endsWithSeparator, setEndsWithSeparator] = useState(false)
+  const value = fieldValue ?? slugify(source)
+  const inputValue = value + (endsWithSeparator ? '-' : '')
+
+  function handleChange(next: string) {
+    setEndsWithSeparator(isSeparator(next.charAt(next.length - 1)))
+    onChange(slugify(next))
+  }
+
+  function handleBlur() {
+    setEndsWithSeparator(false)
+  }
+
+  return (
+    <TextField
+      description={description}
+      errorMessage={errorMessage}
+      icon={<IcRoundLink />}
+      isInvalid={isInvalid}
+      isReadOnly={isReadOnly}
+      isRequired={isRequired}
+      label={label}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      value={inputValue}
+    />
+  )
+}
