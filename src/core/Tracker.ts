b27344addbbf7a4f84e1c4375a16c09103964303
Ben Merckx <ben@codeurs.be>
2026-05-22 14:34:05 +0200

    Remove value trackers api, that was never supposed to be released

--- src/core/Tracker.ts
+++ src/core/Tracker.ts
@@ -7,21 +7,10 @@ export interface FieldGetter {
 export interface OptionsTracker<Options = any> {
   (getter: FieldGetter): Partial<Options>
 }
-
-export interface ValueTracker<Value = any> {
-  (getter: FieldGetter): Value
-}
-
 const optionTrackers = new Map<symbol, OptionsTracker>()
 
 export function optionTrackerOf(field: Field) {
   return optionTrackers.get(Field.ref(field))
-}
-
-const valueTrackers = new Map<symbol, ValueTracker>()
-
-export function valueTrackerOf(field: Field) {
-  return valueTrackers.get(Field.ref(field))
 }
 
 export namespace track {
@@ -30,14 +19,6 @@ export namespace track {
     tracker: OptionsTracker<Options & FieldOptions<StoredValue>>
   ): Field<StoredValue, QueryValue, OnChange, Options> {
     optionTrackers.set(Field.ref(field), tracker)
-    return field
-  }
-
-  export function value<StoredValue, QueryValue, OnChange, Options>(
-    field: Field<StoredValue, QueryValue, OnChange, Options>,
-    tracker: ValueTracker<StoredValue>
-  ): Field<StoredValue, QueryValue, OnChange, Options> {
-    valueTrackers.set(Field.ref(field), tracker)
     return field
   }
 }
