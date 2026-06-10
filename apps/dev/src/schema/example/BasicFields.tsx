1543269aee75deb8268e92dbfcc494f2cdb13d7b
Stijn Van Doorslaer <stijn@codeurs.be>
2026-06-10 09:46:01 +0200

    Example changes

--- apps/dev/src/schema/example/BasicFields.tsx
+++ apps/dev/src/schema/example/BasicFields.tsx
@@ -1,13 +1,15 @@
-import {Config, Field} from 'alinea'
+import { Config, Field } from 'alinea'
 
 export const BasicFields = Config.document('Basic fields', {
   fields: {
-    path: Field.path('Locked path', {
-      readOnly: true
+    path: Field.path('Path (read-only)', {
+      readOnly: true,
+      required: true,
+      width: 0.5,
     }),
     text: Field.text('Text field'),
-    hello: Field.text('Validated text field', {
-      help: 'This field only accepts "hello"',
+    hello: Field.text('Text field with validation', {
+      help: <p>This field only accepts "<b>hello</b>"</p>,
       validate: value => {
         if (value !== 'hello') {
           return 'Only "hello" is allowed'
@@ -16,7 +18,6 @@ export const BasicFields = Config.document('Basic fields', {
     }),
     richText: Field.richText('Rich text field', {enableTables: true}),
     select: Field.select('Select field', {
-      width: 0.5,
       options: {
         a: 'Option a',
         b: 'Option b'
@@ -25,15 +26,11 @@ export const BasicFields = Config.document('Basic fields', {
     number: Field.number('Number field', {
       minValue: 0,
       maxValue: 10,
-      help: (
-        <p>
-          A help text can contain <b>markup</b>
-        </p>
-      )
+      help: 'minValue: 0, maxValue: 10'
     }),
     check: Field.check('Check field', {description: 'Check me please'}),
-    date: Field.date('Date field'),
-    time: Field.time('Time field'),
+    date: Field.date('Date field', {width: 0.5}),
+    time: Field.time('Time field', {width: 0.5}),
     code: Field.code('Code field'),
     object: Field.object('Object', {
       help: 'An object field',
