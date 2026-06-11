2b4cfbd377b008d59106fcd0cd714161517c4736
Ben Merckx <ben@codeurs.be>
2026-06-11 12:02:47 +0200

    Add one more icon path

---
+++ src/ui/icons/IcRoundPanorama.tsx
@@ -0,0 +1,6 @@
+import {IcRoundPanorama as Icon} from 'alinea/dashboard/icons'
+
+/** @deprecated Import from `alinea/dashboard/icons` instead. */
+export const IcRoundPanorama = Icon
+
+export default IcRoundPanorama
