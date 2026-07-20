ee22af3b0ede04db46e4589d345e4474df6fae56
vctrth <thysvic@gmail.com>
2026-07-20 16:39:03 +0200

    formating issue

--- src/dashboard/app/AppShell.tsx
+++ src/dashboard/app/AppShell.tsx
@@ -15,7 +15,7 @@ import {Sidebar, SidebarHeader} from './ui/Sidebar.js'
 import {UsersPage, UsersPageSidebar} from './UsersPage.js'
 import {WorkspaceMenu} from './WorkspaceMenu.js'
 import {WorkspaceRoots} from './WorkspaceRoots.js'
-import {ToastContainer, BlockingToast } from './ToastContainer.js'
+import {ToastContainer, BlockingToast} from './ToastContainer.js'
 import {ToastRouter} from './ToastRouter.js'
 
 const styles = styler(css)
